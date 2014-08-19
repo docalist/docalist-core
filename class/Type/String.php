bb4efad46e8718b3b4ce3821d7785fa711cdc22b
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-19 08:13:41 +0000

    String

---
+++ class/Type/String.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of a "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+namespace Docalist\Type;
+
+use Docalist\Type\Exception\InvalidTypeException;
+
+/**
+ * Type chaine de caractères.
+ */
+class String extends Scalar {
+    static protected $default = '';
+
+    public function assign($value) {
+        if (! is_string($value)){
+            throw new InvalidTypeException('string');
+        }
+
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function __toString() {
+        return json_encode($this->value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    }
+}
