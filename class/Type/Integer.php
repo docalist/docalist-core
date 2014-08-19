412c5dc009194dedf93d1789c8e350f5764a8906
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-19 08:13:26 +0000

    Integer

---
+++ class/Type/Integer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of a "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+namespace Docalist\Type;
+
+use Docalist\Type\Exception\InvalidTypeException;
+
+/**
+ * Type entier.
+ */
+class Integer extends Scalar {
+    static protected $default = 0;
+
+    public function assign($value) {
+        if (! is_int($value)){
+            throw new InvalidTypeException('int');
+        }
+
+        $this->value = $value;
+
+        return $this;
+    }
+}
