925603ef51810c751f57c34d2be4bb6496426ab5
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-19 08:12:52 +0000

    Boolean

---
+++ class/Type/Boolean.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of a "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+namespace Docalist\Type;
+
+use Docalist\Type\Exception\InvalidTypeException;
+
+/**
+ * Type booléen.
+ */
+class Boolean extends Scalar {
+    static protected $default = true;
+
+    public function assign($value) {
+        if (! is_bool($value)){
+            throw new InvalidTypeException('boolean');
+        }
+
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->value ? 'true' : 'false';
+    }
+}
