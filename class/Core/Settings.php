7b93025b4480901f466e4635dd3368e48001b1a2
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-20 04:06:29 +0000

    Settings : adaptation aux modifs.

--- class/Core/Settings.php
+++ class/Core/Settings.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Core' plugin.
  *
- * Copyright (C) 2012, 2013 Daniel Ménard
+ * Copyright (C) 2012-2014 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -14,7 +14,7 @@
  */
 namespace Docalist\Core;
 
-use Docalist\Data\Entity\AbstractSettingsEntity;
+use Docalist\Type\Settings as TypeSettings;
 use Docalist\Table\TableInfo;
 
 /**
@@ -22,14 +22,16 @@ use Docalist\Table\TableInfo;
  *
  * @property Docalist\Table\TableInfo[] $tables Liste des tables.
  */
-class Settings extends AbstractSettingsEntity
-{
-    protected function loadSchema() {
+class Settings extends TypeSettings {
+    protected $id = 'docalist-core-settings';
+
+    protected static function loadSchema() {
         return array(
-            'tables' => array(
+            'tables' => [
                 'type' => 'Docalist\Table\TableInfo*',
+                'key' => 'name',
                 'label' => __('Liste des tables d\'autorité personnalisées', 'docalist-core'),
-            ),
+            ],
         );
     }
 }
