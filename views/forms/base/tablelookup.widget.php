6f07ce738b9c5ac6dbb799120929802c23424ab3
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-21 07:48:27 +0000

    Typo.

--- views/forms/base/tablelookup.widget.php
+++ views/forms/base/tablelookup.widget.php
@@ -16,7 +16,7 @@ $labelField = $this->labelField();
 // 1. Récupère le contenu actuel du champ
 if ($this->data instanceof Docalist\Type\Collection) {
     // par exemple si on a passé un objet "Settings" ou Property comme valeur actuelle du champ
-    $data = $this->data->value()
+    $data = $this->data->value();
 } else {
     $data = (array)$this->data;
 }
