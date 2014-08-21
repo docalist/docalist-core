353121262eb1c1b1e2f67cad696952a2394d541a
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-21 06:13:04 +0000

    Tests unitaires pour la classe Boolean.

---
+++ tests/Docalist/Type/BooleanTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Tests
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+namespace Docalist\Tests\Type;
+
+use WP_UnitTestCase;
+
+use Docalist\Type\Boolean;
+
+class BooleanTest extends WP_UnitTestCase {
+    public function testNew() {
+        $a = new Boolean();
+        $this->assertSame(true, $a->value());
+    }
+
+    /** @expectedException Docalist\Type\Exception\InvalidTypeException */
+    public function testInvalidType()
+    {
+        new Boolean('true');
+    }
+
+    public function testToString() {
+        $t = new Boolean(true);
+        $f = new Boolean(false);
+
+        $this->assertSame('true', $t->__toString());
+        $this->assertSame('false', $f->__toString());
+    }
+}
