7e9363dfb8dfc46254dd032dc27f5e2479f9dc6c
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-09-26 17:30:28 +0000

    TopicsInput : Un widget spécialisé pour la saisie du champ topic de docalist-biblio.
    TODO: Ne devrait pas être là mais pour le moment, la surcharge des vues de formulaires ne marche pas. Il faudrait faire un theme wordpressbiblio qui étend wordpress, mais comme wordpress étend déjà base, on se retrouve avec trois niveaux et actuellement ce n'est pas géré.

---
+++ class/Forms/TopicsInput.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of the "Docalist Forms" package.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Forms
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+namespace Docalist\Forms;
+
+/**
+ * Un widget spécialisé pour la saisie du champ topic de docalist-biblio.
+ *
+ * TODO: Ne devrait pas être là mais pour le moment, la surcharge des vues de
+ * formulaires ne marche pas. Il faudrait faire un theme wordpressbiblio qui
+ * étend wordpress, mais comme wordpress étend déjà base, on se retrouve avec
+ * trois niveaux et actuellement ce n'est pas géré.
+ *
+ */
+class TopicsInput extends Fragment {
+    protected $table;
+
+    public function __construct($name = null, $table = null) {
+        parent::__construct($name);
+        $this->table = $table;
+    }
+
+    public function repeatable($repeatable = null) {
+        return false;
+    }
+}
