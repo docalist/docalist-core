369d6fac1b068706e5103f152593fde52a597498
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-04-09 09:47:31 +0000

    ConfigRepository : utilise docalist('config-dir")

--- class/Repository/ConfigRepository.php
+++ class/Repository/ConfigRepository.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * This file is part of a "Docalist Core" plugin.
+ * This file is part of the "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
  *
- * @package Docalist
- * @subpackage Core
- * @author Daniel Ménard <daniel.menard@laposte.net>
- * @version SVN: $Id$
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
  */
 namespace Docalist\Repository;
 
@@ -25,6 +27,6 @@ class ConfigRepository extends DirectoryRepository {
      * n'est indiqué lors de l'appel.
      */
     public function __construct($type = 'Docalist\Type\Entity') {
-        parent::__construct(docalist('site-root') . 'config', $type);
+        parent::__construct(docalist('config-dir'), $type);
     }
 }
