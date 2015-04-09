12c76905822aa85a3cf77b0eab1f774c722a7bed
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-04-09 13:46:19 +0000

    TableInterface : update header

--- class/Table/TableInterface.php
+++ class/Table/TableInterface.php
@@ -2,15 +2,15 @@
 /**
  * This file is part of a "Docalist Core" plugin.
  *
- * Copyright (C) 2012, 2013 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
  *
- * @package Docalist
- * @subpackage Core
- * @author Daniel Ménard <daniel.menard@laposte.net>
- * @version SVN: $Id$
+ * @package     Docalist
+ * @subpackage  Table
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
  */
 namespace Docalist\Table;
 
