afc1fb271715085e62026288b7d0137568ec3335
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-04-09 13:50:10 +0000

    CsvTable : adaptation

--- class/Table/CsvTable.php
+++ class/Table/CsvTable.php
@@ -2,15 +2,15 @@
 /**
  * This file is part of a "Docalist Core" plugin.
  *
- * Copyright (C) 2012, 2013 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
  *
- * @package Docalist
- * @subpackage Core
- * @author Daniel Ménard <daniel.menard@laposte.net>
- * @version SVN: $Id$
+ * @package     Docalist
+ * @subpackage  Table
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
  */
 namespace Docalist\Table;
 
@@ -52,8 +52,7 @@ class CsvTable extends SQLite {
 
         $sql = $this->parseFields();
 
-        $this->db = $this->createSQLiteDatabase($path, $sql);
-        $this->commit = true;
+        $this->createSQLiteDatabase($path, $sql);
 
         // Prépare le statement utilisé pour charger les données
         $sql = sprintf
