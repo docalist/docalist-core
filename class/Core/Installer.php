faa5d954487d39acc2909e3598a197a5b05b72fe
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-04-13 06:35:39 +0000

    Nouvelle classe Installer pour docalist-core.
    Lors de l'activation du plugin, enregistre toutes les tables prédéfinies et les supprime lorsque le plugin est désactivé.

---
+++ class/Core/Installer.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * This file is part of the "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012-2015 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+namespace Docalist\Core;
+
+use Docalist\Table\TableManager;
+use Docalist\Table\TableInfo;
+/**
+ * Installation/désinstallation de docalist-core.
+ */
+class Installer {
+
+    /**
+     * Initialise l'installateur.
+     */
+    public function __construct() {
+        // Charge le plugin docalist-core si ce n'est pas encore fait : quand wp
+        // exécute "plugin_sandbox_scrape", plugins_loaded n'a pas encore été appellé
+        if (! docalist('services')->has('docalist-core')) {
+            docalist('services')->add('docalist-core', new Plugin());
+        }
+    }
+
+    /**
+     * Activation : enregistre les tables prédéfinies.
+     *
+     */
+    public function activate() {
+        $tableManager = docalist('table-manager'); /* @var $tableManager TableManager */
+
+        // Enregistre les tables prédéfinies
+        foreach($this->tables() as $name => $table) {
+            $table['name'] = $name;
+            $tableManager->register(new TableInfo($table));
+        }
+    }
+
+    /**
+     * Désactivation : supprime les tables prédéfinies.
+     */
+    public function deactivate() {
+        $tableManager = docalist('table-manager'); /* @var $tableManager TableManager */
+
+        // Supprime les tables prédéfinies
+        foreach(array_keys($this->tables()) as $table) {
+            $tableManager->unregister($table);
+        }
+    }
+
+
+    /**
+     * Retourne la liste des tables prédéfinies.
+     *
+     * @return array
+     */
+    protected function tables() {
+        $dir = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'tables'  . DIRECTORY_SEPARATOR;
+        return [
+            // Tables des langues complète
+            'ISO-639-2_alpha3_fr' => [
+                'path' => $dir . 'languages/ISO-639-2_alpha3_fr.txt',
+                'label' => __('Liste complète des codes langues 3 lettres en français (ISO-639-2)', 'docalist-core'),
+                'format' => 'table',
+                'type' => 'languages',
+            ],
+            'ISO-639-2_alpha3_en' => [
+                'path' => $dir . 'languages/ISO-639-2_alpha3_en.txt',
+                'label' => __('Liste complète des codes langues 3 lettres en anglais (ISO-639-2)', 'docalist-core'),
+                'format' => 'table',
+                'type' => 'languages',
+            ],
+
+            // Tables des langues simplifiées (langues officielles de l'union européenne)
+            'ISO-639-2_alpha3_EU_fr' => [
+                'path' => $dir . 'languages/ISO-639-2_alpha3_EU_fr.txt',
+                'label' => __('Codes 3 lettres en français des langues officielles de l\'Union Européenne (ISO-639-2)', 'docalist-core'),
+                'format' => 'table',
+                'type' => 'languages',
+            ],
+            'ISO-639-2_alpha3_EU_en' => [
+                'path' => $dir . 'languages/ISO-639-2_alpha3_EU_en.txt',
+                'label' => __('Codes 3 lettres en anglais des langues officielles de l\'Union Européenne (ISO-639-2)', 'docalist-core'),
+                'format' => 'table',
+                'type' => 'languages',
+            ],
+
+            // Tables de conversion des codes langues
+            'ISO-639-2_alpha2-to-alpha3' => [
+                'path' => $dir . 'languages/ISO-639-2_alpha2-to-alpha3.txt',
+                'label' => __('Table de conversion "alpha2 -> alpha3" pour les codes langues (ISO-639-2)', 'docalist-core'),
+                'format' => 'conversion',
+                'type' => 'languages',
+            ],
+
+            // Tables des pays
+            'ISO-3166-1_alpha2_fr' => [
+                'path' => $dir . 'countries/ISO-3166-1_alpha2_fr.txt',
+                'label' => __('Codes pays 2 lettres en français (ISO-3166-1)', 'docalist-core'),
+                'format' => 'table',
+                'type' => 'countries',
+            ],
+            'ISO-3166-1_alpha2_EN' => [
+                'path' => $dir . 'countries/ISO-3166-1_alpha2_en.txt',
+                'label' => __('Codes pays 2 lettres en anglais (ISO-3166-1)', 'docalist-core'),
+                'format' => 'table',
+                'type' => 'countries',
+            ],
+            'ISO-3166-1_alpha3-to-alpha2' => [
+                'path' => $dir . 'countries/ISO-3166-1_alpha3-to-alpha2.txt',
+                'label' => __('Table de conversion "alpha3 -> alpha2" pour les codes pays (ISO-3166-1)', 'docalist-core'),
+                'format' => 'conversion',
+                'type' => 'countries',
+            ],
+        ];
+    }
+}
