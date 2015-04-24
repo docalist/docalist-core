80c0a8d60f1125410ac0344510373c8c8c73c43d
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-04-24 17:08:10 +0000

    Corrige le bug #262 (plus de lookups).
    https://github.com/daniel-menard/prisme/issues/262

--- class/Forms/Themes.php
+++ class/Forms/Themes.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Forms" package.
  *
- * Copyright (C) 2012,2013 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -103,6 +103,7 @@ class Themes {
                 'assets' => array(
                     array(
                         'name' => 'docalist-forms',
+                        'position' => Assets::TOP
                     ),
                 ),
             ),
