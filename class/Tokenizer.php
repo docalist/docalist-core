6f8a8290825edfea5f59167dd29e8bae8eaed3e6
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-06-17 09:59:31 +0000

    Tokenizer : ajout de la méthode uppercaseNoAccents() et de la table de conversion associée.
    + passe les tables en protected (n'ont pas lieu d'être public)

--- class/Tokenizer.php
+++ class/Tokenizer.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Core" plugin.
  *
- * Copyright (C) 2012, 2013 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -12,21 +12,22 @@
  * @author      Daniel Ménard <daniel.menard@laposte.net>
  * @version     SVN: $Id$
  */
-
 namespace Docalist;
 
 /**
  * Tokenizer
  *
  * Convertit un texte en minuscules non accentuées
+ *
+ * TODO : transformer cette classe statique en service de gestion de texte.
  */
 class Tokenizer {
     /**
-     * Table de conversion des caractères.
+     * Table de conversion des caractères pour le tokenizer.
      *
      * @var array
      */
-    public static $map = [
+    protected static $tokenizer = [
         // U0000 - Latin de base (http://fr.wikipedia.org/wiki/Table_des_caractères_Unicode/U0000)
         'A' => 'a',    'B' => 'b',    'C' => 'c',    'D' => 'd',    'E' => 'e',    'F' => 'f',
         'G' => 'g',    'H' => 'h',    'I' => 'i',    'J' => 'j',    'K' => 'k',    'L' => 'l',
@@ -87,11 +88,87 @@ class Tokenizer {
         "'" => ' ',    '-' => ' ',
     ];
 
+    /**
+     * Table de conversion des caractères pour uppercaseNoAccents().
+     *
+     * @var array
+     */
+    protected static $uppercaseNoAccents = [
+        // U0000 - Latin de base (http://fr.wikipedia.org/wiki/Table_des_caractères_Unicode/U0000)
+        'a' => 'A',    'b' => 'B',    'c' => 'C',    'd' => 'D',    'e' => 'E',    'f' => 'F',
+        'g' => 'G',    'h' => 'H',    'i' => 'I',    'j' => 'J',    'k' => 'K',    'l' => 'L',
+        'm' => 'M',    'n' => 'N',    'o' => 'O',    'p' => 'P',    'q' => 'Q',    'r' => 'R',
+        's' => 'S',    't' => 'T',    'u' => 'U',    'v' => 'V',    'w' => 'W',    'x' => 'X',
+        'y' => 'Y',    'z' => 'Z',
+
+        // U0080 - Supplément Latin-1 (http://fr.wikipedia.org/wiki/Table_des_caractères_Unicode/U0080)
+        'À' => 'A',    'Á' => 'A',    'Â' => 'A',    'Ã' => 'A',    'Ä' => 'A',    'Å' => 'A',
+        'Æ' => 'AE',   'Ç' => 'C',    'È' => 'E',    'É' => 'E',    'Ê' => 'E',    'Ë' => 'E',
+        'Ì' => 'I',    'Í' => 'I',    'Î' => 'I',    'Ï' => 'I',    'Ð' => 'D',    'Ñ' => 'N',
+        'Ò' => 'O',    'Ó' => 'O',    'Ô' => 'O',    'Õ' => 'O',    'Ö' => 'O',    'Ø' => 'O',
+        'Ù' => 'U',
+        'Ú' => 'U',    'Û' => 'U',    'Ü' => 'U',    'Ý' => 'Y',    'Þ' => 'TH',   'ß' => 'SS',
+        'à' => 'A',    'á' => 'A',    'â' => 'A',    'ã' => 'A',    'ä' => 'A',    'å' => 'A',
+        'æ' => 'AE',   'ç' => 'C',    'è' => 'E',    'é' => 'E',    'ê' => 'E',    'ë' => 'E',
+        'ì' => 'I',    'í' => 'I',    'î' => 'I',    'ï' => 'I',    'ð' => 'D',    'ñ' => 'N',
+        'ò' => 'O',    'ó' => 'O',    'ô' => 'O',    'õ' => 'O',    'ö' => 'O',    'ø' => 'O',
+        'ù' => 'U',    'ú' => 'U',    'û' => 'U',    'ü' => 'U',    'ý' => 'Y',    'þ' => 'TH',
+        'ÿ' => 'Y',
+
+        // U0100 - Latin étendu A (http://fr.wikipedia.org/wiki/Table_des_caractères_Unicode/U0100)
+        'Ā' => 'A',    'ā' => 'A',    'Ă' => 'A',    'ă' => 'A',    'Ą' => 'A',    'ą' => 'A',
+        'Ć' => 'C',    'ć' => 'C',    'Ĉ' => 'C',    'ĉ' => 'C',    'Ċ' => 'C',    'ċ' => 'C',
+        'Č' => 'C',    'č' => 'C',    'Ď' => 'D',    'ď' => 'D',    'Đ' => 'D',    'đ' => 'D',
+        'Ē' => 'E',    'ē' => 'E',    'Ĕ' => 'E',    'ĕ' => 'E',    'Ė' => 'E',    'ė' => 'E',
+        'Ę' => 'E',    'ę' => 'E',    'Ě' => 'E',    'ě' => 'E',    'Ĝ' => 'G',    'ĝ' => 'G',
+        'Ğ' => 'G',    'ğ' => 'G',    'Ġ' => 'G',    'ġ' => 'G',    'Ģ' => 'G',    'ģ' => 'G',
+        'Ĥ' => 'H',    'ĥ' => 'H',    'Ħ' => 'H',    'ħ' => 'H',    'Ĩ' => 'I',    'ĩ' => 'I',
+        'Ī' => 'I',    'ī' => 'I',    'Ĭ' => 'I',    'ĭ' => 'I',    'Į' => 'I',    'į' => 'I',
+        'İ' => 'J',    'ı' => 'I',    'Ĳ' => 'IJ',   'ĳ' => 'IJ',   'Ĵ' => 'J',    'ĵ' => 'J',
+        'Ķ' => 'K',    'ķ' => 'K',    'ĸ' => 'K',    'Ĺ' => 'L',    'ĺ' => 'L',    'Ļ' => 'L',
+        'ļ' => 'L',    'Ľ' => 'L',    'ľ' => 'L',    'Ŀ' => 'L',    'ŀ' => 'L',    'Ł' => 'L',
+        'ł' => 'L',    'Ń' => 'N',    'ń' => 'N',    'Ņ' => 'N',    'ņ' => 'N',    'Ň' => 'N',
+        'ň' => 'N',    'ŉ' => 'N',    'Ŋ' => 'N',    'ŋ' => 'N',    'Ō' => 'O',    'ō' => 'O',
+        'Ŏ' => 'O',    'ŏ' => 'O',    'Ő' => 'O',    'ő' => 'O',    'Œ' => 'OE',   'œ' => 'OE',
+        'Ŕ' => 'R',    'ŕ' => 'R',    'Ŗ' => 'R',    'ŗ' => 'R',    'Ř' => 'R',    'ř' => 'R',
+        'Ś' => 'S',    'ś' => 'S',    'Ŝ' => 'S',    'ŝ' => 'S',    'Ş' => 'S',    'ş' => 'S',
+        'Š' => 'S',    'š' => 'S',    'Ţ' => 'T',    'ţ' => 'T',    'Ť' => 'T',    'ť' => 'T',
+        'Ŧ' => 'T',    'ŧ' => 'T',    'Ũ' => 'U',    'ũ' => 'U',    'Ū' => 'U',    'ū' => 'U',
+        'Ŭ' => 'U',    'ŭ' => 'U',    'Ů' => 'U',    'ů' => 'U',    'Ű' => 'U',    'ű' => 'U',
+        'Ų' => 'U',    'ų' => 'U',    'Ŵ' => 'W',    'ŵ' => 'W',    'Ŷ' => 'Y',    'ŷ' => 'Y',
+        'Ÿ' => 'Y',    'Ź' => 'Z',    'ź' => 'Z',    'Ż' => 'Z',    'ż' => 'Z',    'Ž' => 'Z',
+        'ž' => 'Z',    'ſ' => 'S',
+
+        // U0180 - Latin étendu B (http://fr.wikipedia.org/wiki/Table_des_caractères_Unicode/U0180)
+        // Voir ce qu'il faut garder : slovène/croate, roumain,
+        'Ș' => 'S',    'ș' => 'S',    'Ț' => 'T',    'ț' => 'T',   // Supplément pour le roumain
+
+        // U20A0 - Symboles monétaires (http://fr.wikipedia.org/wiki/Table_des_caractères_Unicode/U20A0)
+        // '€' => 'E',
+
+        // autres symboles monétaires : Livre : 00A3 £, dollar 0024 $, etc.
+    ];
+
     public static function tokenize($text)
     {
-        $text = strtr($text, self::$map);
+        $text = strtr($text, self::$tokenizer);
         $text = str_word_count($text, 1, '0123456789@_');
 
         return $text;
+    }
+
+    /**
+     * Convertit le texte en majuscules en remplaçant les majuscules accentuées
+     * par la lettre majuscule de base.
+     *
+     * Remarque : remplace également les ligatures par les lettres
+     * correspondantes.
+     *
+     * @param string $text
+     * @return string
+     */
+    public static function uppercaseNoAccents($text)
+    {
+        return strtr($text, self::$uppercaseNoAccents);
     }
 }
