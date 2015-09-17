d760d161817101d8d4354e4b0a77ab2591058391
Daniel Ménard <daniel.menard@laposte.net>
2015-09-17 09:53:32 +0200

    Types : renomme la classe Float en Decimal
    docalist/docalist#349

--- class/Type/Any.php
+++ class/Type/Any.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of a "Docalist Core" plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -264,7 +264,7 @@ class Any implements Serializable, JsonSerializable {
         }
 
         if (is_float($value)) {
-            return new Float($value);
+            return new Decimal($value);
         }
 
         if (is_null($value)) {
--- class/Type/Decimal.php
+++ class/Type/Decimal.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of a "Docalist Core" plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -18,7 +18,7 @@ use Docalist\Type\Exception\InvalidTypeException;
 /**
  * Type nombre décimal.
  */
-class Float extends Scalar {
+class Decimal extends Scalar {
     static protected $default = 0.0;
 
     public function assign($value) {
