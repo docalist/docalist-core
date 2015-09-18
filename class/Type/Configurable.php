078b94e010dfccff9986528e4c14403905f55328
Daniel Ménard <daniel.menard@laposte.net>
2015-09-18 23:48:51 +0200

    Nouvelle interface Configurable : paramétrage d'un type docalist

---
+++ class/Type/Configurable.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of a "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012-2015 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Type;
+
+use Docalist\Forms\Field;
+
+/**
+ * Configurable : API permettant de modifier les paramètres d'un type de données
+ * docalist.
+ *
+ * Tous les types de données docalist disposent peuvent être paramétrés. On peut
+ * par exemple choisir le libellé à utiliser, la table d'autorité associée, les
+ * droits requis pour y avoir accès, etc.
+ *
+ * La méthode {@link getSettingsForm()} retourne un {@link Docalist\Forms\Field
+ * formulaire} contenant toutes les options disponibles.
+ *
+ * Les données saisies par l'utilisateur dans ce formulaire peuvent être
+ * validées en utilisant la méthode {@link validateSettings()}.
+ */
+interface Configurable {
+    /**
+     * Retourne formulaire permettant de saisir et de modifier
+     * les paramètres de base du type : libellé à utiliser, description, droit
+     * requis, etc.
+     *
+     * @return Fragment
+     */
+    public function getSettingsForm();
+
+    /**
+     * Valide les paramètres de base du type.
+     *
+     * Typiquement, cette méthode est utilisée pour valider les paramètres
+     * saisis par l'utilisateur dans le formulaire généré par
+     * {@link settingsForm()}.
+     *
+     * Par défaut, la méthode ne fait rien (elle retourne les paramètres
+     * inchangés) mais les classes descendantes peuvent surcharger cette méthode
+     * pour faire les vérifications nécessaires.
+     *
+     * @param array $settings Les paramétres validés.
+     *
+     * @return array
+     */
+    public function validateSettings(array $settings);
+}
