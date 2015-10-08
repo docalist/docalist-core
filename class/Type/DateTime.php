0c494d029dc973d0a3994c0e15ca3348bb83fd5f
Daniel Ménard <daniel.menard@laposte.net>
2015-10-08 17:30:26 +0200

    Nouvelle classe Type\DateTime
    Pour le moment, c'est juste un type Text. Par la suite, on pourra
    implémenter un datetime picker (quand ce sera dispo dans
    docalist-forms).

---
+++ class/Type/DateTime.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of a "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012-2015 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Type;
+
+/**
+ * Une date/heure stockée sous forme de chaine au format 'yyyy-MM-dd HH:mm:ss'.
+ *
+ * Exemple : "2014-09-02 11:19:24"
+ */
+class DateTime extends Text
+{
+    // générer un date/time picker pour getEditorForm()
+}
