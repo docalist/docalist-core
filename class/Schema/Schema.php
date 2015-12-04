676093f9171ad409fe7155b0317cf1adc87e9a3a
Daniel Ménard <daniel.menard@laposte.net>
2015-12-04 17:32:23 +0100

    Nouvelle version de la classe Schema
    - la classe est maintenant un Composite, ce qui simplifie beaucoup le
    code
    - On ne fait plus la distinction entre schéma d'entité et schéma de
    champs, on a juste des schemas (la classe Schema\Field disparaît).
    - beaucoup de choses ont été simplifiées : plus de type par défaut, plus
    de contrôles sur les propriétés, etc.
    
    Remarque : la classe sera par la suite renommée en Docalist\Schema
    (actuellement, le nom complet, c'est Docalist\Schema\Schema).

--- class/Schema/Schema.php
+++ class/Schema/Schema.php
@@ -13,152 +13,190 @@
  */
 namespace Docalist\Schema;
 
+use Docalist\Type\Composite;
+use Docalist\Type\Collection;
 use InvalidArgumentException;
-use Docalist\Type\Any;
 
 /**
- * Un schéma permet de décrire les attributs d'un {@link Docalist\Type\Any type
- * de données Docalist}.
+ * Un schéma est un {@link Docalist\Type\Composite objet composite} qui permet de
+ * décrire les attributs d'un {@link Docalist\Type\Any type de données Docalist}.
  *
- * Pour un {@link Docalist\Type\Composite type structuré}, un schéma permet
- * également de définir la liste des champs qui composent cette structure en
- * utilisant la collection fields.
+ * Sur le principe, c'est juste un moyen simple de stocker une liste de propriétés
+ * de la forme clé => valeur.
  *
- * Les attributs d'un schéma peuvent être manipulés comme s'il s'agissait de
- * propriétés ou de méthodes du schéma :
+ * La plupart des propriétés sont libres (il faut juste que ce soit des scalaires)
+ * mais certaines propriétés connues sont contrôlées.
  *
- * <code>
- *     $schema->description = 'Un exemple de schéma';
- *     echo $schema->description;       // propriété
- *     echo $schema->description();     // méthode
- * </code>
+ * Un schéma peut également avoir une propriété 'fields' qui décrit les propriétés
+ * des sous-champs d'un type composite. Dans ce cas, chaque élément de la collection
+ * fields sera elle-même un schéma (i.e. la structure obtenue est récursive).
+ *
+ * Les schémas sont notamment utilisés pour définir la liste des champs des composites
+ * et pour définir les grilles (affichage, saisie, etc.) des entités.
  */
-class Schema extends Any {
+class Schema extends Composite
+{
+    // Alias courts pour les types de base docalist (deprecated)
+    private static $alias = [
+        'any' => 'Docalist\Type\Any',
 
-/*
-    Réflexion : bootstraper ?
+        'bool' => 'Docalist\Type\Boolean',
+        'boolean' => 'Docalist\Type\Boolean',
 
-    Actuellement, les propriétés du schéma sont stockées sous la forme de types
-    php natifs et fields est un simple tableau php.
+        'float' => 'Docalist\Type\Decimal',
+        'double' => 'Docalist\Type\Decimal',
+        'real' => 'Docalist\Type\Decimal',
 
-    Pour être cohérent il faudrait les stocker sous la forme de types docalist
-    et non de types php : fields serait une Collection de Field et les
-    propriétés scalaires (description, label...) seraient stockées avec le type
-    docalist adéquat (Text, etc.)
+        'int' => 'Docalist\Type\Integer',
+        'integer' => 'Docalist\Type\Integer',
+        'long' => 'Docalist\Type\Integer',
 
-    Cela simplifierait pas mal les choses, car dans ce cas, la classe Schema
-    serait juste un objet standard (au lieu d'hériter de Any, on hériterait de
-    Composite). :
+        'scalar' => 'Docalist\Type\Scalar',
 
-    - Les méthodes __get, __set, __isset, __unset,  __call pourraient être
-      supprimées (déjà implémentées par Composite).
-    - La méthode field() pourrait être supprimée (propriété comme une autre)
-    - $schema->fieldNames() pourrait être remplacé par $schema->fields->keys()
-    - $schema->field($field) pourrait être remplacé par $schema->field[$field]
-    - has($field) pourrait être remplacé par isset($schema->field[$field])
-
-    C'est assez séduisant (c'est toujours le cas quand on bootstrappe !) mais
-    est-utile ?
-
-    Au lieu de créer des types php, il faudra crééer des objets pour chaque
-    propriété. Quel impact sur les performances ?
-
-    Si au final on fait ça, il y a un impact un peu partout sur le code actuel.
-    Il faut revoir tous les appels aux propriétés/méthodes des schémas et
-    disposer de tests unitaires pour vérifier qu'on ne casse rien.
- */
+        'string' => 'Docalist\Type\Text',
+        'text' => 'Docalist\Type\Text',
+    ];
 
     /**
      * Construit un nouveau schéma.
      *
      * @param array $value Propriétés du schéma.
      *
-     * @param $defaultNamespace Namespace en cours, utilisé pour résoudre les
-     * noms de classes relatifs dans les champs.
-     *
      * @throws InvalidArgumentException Si le schéma contient des erreurs.
      */
-    public function __construct(array $value) {
-        if (isset($value['fields'])) {
-            $fields = [];
-            foreach ($value['fields'] as $key => $field) {
-                // Si $field est une chaine, on a soit int => name, soit name => type
-                if (is_string($field)) {
-                    $field = is_int($key) ? ['name' => $field] : ['name' => $key, 'type' => $field];
-                }
+    public function __construct(array $value)
+    {
+        // Valide le type
+        if (isset($value['type'])) {
+            $type = (string) $value['type'];
 
-                // Champ de la forme : nom => array(propriétés)
-                elseif (is_string($key)) {
-                    $field['name'] = $key;
-                }
-
-                // Compile
-                $field = new Field($field);
-
-                // Vérifie que le nom du champ est unique
-                $name = $field->name();
-                if (isset($this->fields[$name])) {
-                    $msg = 'Field %s defined twice';
-                    throw new InvalidArgumentException(sprintf($msg, $name));
-                }
-
-                // Stocke le champ
-                $fields[$name] = $field;
+            // Répétable : type='xx*' équivaut à type='xx' + repeatable=true
+            if (substr($type, -1) === '*') {
+                $type = substr($type, 0, -1);
+                $value['repeatable'] = true;
             }
-            $value['fields'] = $fields;
+
+            // Si le type indiqué est un alias, on le traduit en nom de Type
+            if (isset(self::$alias[$type])) {
+                $type = self::$alias[$type];
+            }
+
+            // Le type indiqué doit être un nom de Type docalist
+            else {
+                if (is_a($type, 'Docalist\Type\Collection', true)) {
+                    $value['collection'] = $type;
+                    $type = $type::type();
+                }
+
+                // La classe indiquée doit être un nom de Type et doit exister
+                if (! is_a($type, 'Docalist\Type\Any', true)) {
+                    $msg = 'Invalid type "%s" for field "%s"';
+
+                    throw new InvalidArgumentException(sprintf($msg, $type, isset($value['name']) ? $value['name'] : '(noname)'));
+                }
+            }
+            $value['type'] = $type;
         }
-        $this->value = $value;
-    }
 
-    /**
-     * Retourne une propriété du schéma.
-     *
-     * @param string $name Nom de la propriété
-     *
-     * @return mixed
-     */
-    public function __call($name, $arguments) {
-        return isset($this->value[$name]) ? $this->value[$name] : null;
-    }
-
-    /**
-     * Retourne une propriété du schéma.
-     *
-     * @param string $name Nom de la propriété
-     *
-     * @return mixed
-     */
-    public function __get($name) {
-        return isset($this->value[$name]) ? $this->value[$name] : null;
-    }
-
-    /**
-     * Modifie une propriété du schéma.
-     *
-     * @param string $name Nom de la propriété
-     * @param mixed $value Valeur de la propriété
-     *
-     * @return mixed
-     */
-    public function __set($name, $value) {
-        if (is_null($value)) {
-            unset($this->value[$name]);
-        } else {
-            $this->value[$name] = $value;
+        // Valide repeatable et collection
+        if (isset($value['repeatable']) && $value['repeatable'] && !isset($value['collection'])) {
+            $value['collection'] = 'Docalist\Type\Collection';
         }
+        unset($value['repeatable']);
+
+        //parent::__construct($value);
+        $this->schema = null;
+        $this->assign($value);
     }
 
-    public function __unset($name) {
-        unset($this->value[$name]);
+    public function __set($name, $value)
+    {
+        // Si la propriété existe déjà, on change simplement sa valeur
+        if (isset($this->value[$name])) {
+            $this->value[$name]->assign($value);
+
+            return $this;
+        }
+
+        // Propriétés génériques
+        if ($name !== 'fields') {
+            $this->value[$name] = self::fromPhpType($value);
+
+            return $this;
+        }
+
+        // Propriété 'fields'
+        if (! is_array($value)) {
+            throw new InvalidArgumentException("Invalid value for property 'fields', expected array");
+        }
+
+        $fields = [];
+        foreach ($value as $key => $field) {
+            // Si $field est une chaine, on a soit int => name, soit name => type
+            if (is_string($field)) {
+                $field = is_int($key) ? ['name' => $field] : ['name' => $key, 'type' => $field];
+            }
+
+            // Champ de la forme : nom => array(propriétés)
+            elseif (is_string($key)) {
+                $field['name'] = $key;
+            }
+
+            // Crée le schéma du champ
+            $field = new self($field);
+
+            // Vérifie que le nom du champ est unique
+            $name = $field->name();
+            if (isset($fields[$name])) {
+                var_dump($fields[$name], $field, $value);
+                throw new InvalidArgumentException("Field $name defined twice");
+            }
+
+            // Stocke le champ
+            $fields[$name] = $field;
+        }
+        $collection = new Collection();
+        $collection->value = $fields;
+        $this->value['fields'] = $collection;
+
+        return $this;
     }
 
     /**
      * Retourne la liste des champs.
      *
-     * @return Field[]
+     * @return Schema[]
      */
-    public function fields() {
-        return isset($this->value['fields']) ? $this->value['fields'] : [];
+    public function getFields()
+    {
+        /*
+            On initialise fields tardivement (plutôt que dans le constructeur)
+            pour pouvoir gérer les schémas récursifs (par exemple un objet Money
+            qui contient des conversions Money*). Si on initialise dès le
+            constructeur, on obtient une boucle infinie sur defaultSchema.
+            Du coup on le fait içi, c'est-à-dire une fois que le format a été
+            chargé et compilé.
+        */
+
+        // Si 'fields' est déjà initialisé, terminé
+        if (array_key_exists('fields', $this->value)) {
+            return is_null($this->value['fields']) ? [] : $this->value['fields']->value;
+        }
+
+        // Si on n'a pas de type ou si n'est pas un composite, impossible d'aller consulter un schéma
+        $type = isset($this->value['type']) ? $this->value['type']->value() : '';
+        if (! is_a($type, 'Docalist\Type\Composite', true)) { // inutile de tester empty($type), is_a le fait
+            $this->value['fields'] = null;
+
+            return [];
+        }
+
+        // Ok, Récupère la liste des champs du composite
+        $this->value['fields'] = $type::defaultSchema()->value['fields'];
+
+        return $this->value['fields']->value;
+
+        /* remarque: c'est la même collection qui est partagée entre plusieurs schémas. Problème ? */
     }
 
     /**
@@ -166,8 +204,9 @@ class Schema extends Any {
      *
      * @return string[]
      */
-    public function fieldNames() {
-        return array_keys($this->fields());
+    public function getFieldNames()
+    {
+        return array_keys($this->getFields());
     }
 
     /**
@@ -180,8 +219,9 @@ class Schema extends Any {
      *
      * @return Field
      */
-    public function field($field) {
-        $fields = $this->fields();
+    public function getField($field)
+    {
+        $fields = $this->getFields();
         if (!isset($fields[$field])) {
             $msg = 'Field %s does not exist';
             throw new InvalidArgumentException(sprintf($msg, $field));
@@ -197,99 +237,37 @@ class Schema extends Any {
      *
      * @return bool
      */
-    public function has($field) {
-        $fields = $this->fields();
+    public function hasField($field)
+    {
+        $fields = $this->getFields();
 
         return isset($fields[$field]);
     }
 
     /**
-     * Convertit le schéma en tableau.
+     * Retourne la valeur par défaut du schéma.
      *
-     * @return array
+     * S'il s'agit d'un schéma simple (sans fields), la méthode retourne le contenu
+     * de la propriété 'default' (ou null si elle n'existe pas).
+     *
+     * Si la propriété 'fields' existe, la méthode retourne un tableau contenant la
+     * valeur par défaut de chacun des champs.
+     *
+     * @return array|scalar|null
      */
-    public function toArray() {
-        $result = $this->value;
-        if (isset($result['fields'])) {
-            $fields = [];
-            foreach ($result['fields'] as $name => $field) {
-                $field = $field->toArray();
-                unset($field['name']);
-                if (empty($field)) {
-                    $fields[] = $name;
-                } elseif (count($field) === 1 && isset($field['type'])) {
-                    $fields[$name] = $field['type'];
-                } else {
-                    $fields[$name] = $field;
+    public function getDefaultValue()
+    {
+        if (isset($this->value['fields'])) {
+            $result = [];
+            foreach ($this->value['fields'] as $name => $field) {
+                if ($name && ! is_null($default = $field->getDefaultValue())) {
+                    $result[$name] = $default;
                 }
             }
-            $result['fields'] = $fields;
+
+            return $result;
         }
 
-        return $result;
-    }
-
-    /**
-     * Fusionne le schéma actuel avec les données passées en paramètre.
-     *
-     * @param array $data
-     */
-    public function merge(array $data) {
-        foreach($data as $key => $value) {
-            if ($key === 'fields') {
-                $this->mergeFields($value);
-            } else {
-                $this->value[$key] = $value;
-                if (empty($this->value[$key])) {
-                    unset($this->value[$key]);
-                }
-            }
-        }
-    }
-
-    private function mergeFields(array $data) {
-        $fields = $this->value['fields'];
-        $result = [];
-        foreach ($data as $name => $data) { // $name = ancien nom du champ
-
-            // le champ existe déjà
-            if (isset($fields[$name])) {
-                $field = $fields[$name];
-                $field->merge($data);
-            }
-
-            // nouveau champ
-            else {
-                $field = new Field($data);
-            }
-
-            // Vérifie que le nom du champ est unique
-            $name = $field->name(); // nouveau nom si renommage
-            if (isset($result[$name])) {
-                $msg = 'Field %s defined twice';
-                throw new InvalidArgumentException(sprintf($msg, $name));
-            }
-
-            $result[$name] = $field;
-        }
-        $this->value['fields'] = $result;
-    }
-
-    /**
-     * Retourne la valeur par défaut du schéma, c'est-à-dire un tableau
-     * contenant la valeur par défaut de tous les champs qui ont une propriété
-     * "default" dans le schéma.
-     *
-     * @return array
-     */
-    public function defaultValue() {
-        $result = [];
-        foreach($this->fields() as $name => $field) {
-            if (! is_null($field->defaultValue())) {
-                $result[$name] = $field->defaultValue(true);
-            }
-        }
-
-        return $result;
+        return isset($this->value['default']) ? $this->value['default'] : null;
     }
 }
