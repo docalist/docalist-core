012ead1c5fbfbc1f582a8bbfc8a27ddf645816b7
Daniel Ménard <daniel.menard@laposte.net>
2015-12-04 12:45:02 +0100

    Nouveau mode de gestion des thèmes de formulaire.
    Jusque là, on avait une grosse classe Themes (statique) qui gérait tout,
    c'était intestable. On avait également notre propre gestion des assets,
    (ça entrait parfois en conflit avec wp) et on utilisait XMLWriter ce qui
    rendait le code très peu lisible.
    
    Désormais, chaque thème est un objet (pas statique) qui s'appuie sur les
    vues docalist pour faire le rendu et dispose des méthodes dont on a
    besoin.
    
    Chaque thème peut générer indifférement du xhtml, du html ou du html5.
    Le code peut être indenté et commenté et les vues sont très simples.
    
    - Theme.php : classe de base pour tous les thèmes
    - Theme/Base.php : thème par défaut
    - Theme/Wordpress.php : thème WordPress
    
    Suppression de Assets.php et de Themes.php

---
+++ class/Forms/Theme/Base.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the "Docalist Forms" package.
+ *
+ * Copyright (C) 2012-2015 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Forms
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Forms\Theme;
+
+use Docalist\Forms\Theme;
+
+/**
+ * Thème de base pour les formulaires.
+ */
+class Base extends Theme
+{
+    protected $styles = [];
+    protected $scripts = ['docalist-forms'];
+
+    public function __construct()
+    {
+        parent::__construct(DOCALIST_CORE . '/views/forms/base');
+    }
+}
