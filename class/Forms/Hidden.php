beda0c56f14e6837b2ca44b1d9552c4c7219cba7
Daniel Ménard <daniel.menard@laposte.net>
2015-12-04 12:08:55 +0100

    Hidden.

--- class/Forms/Hidden.php
+++ class/Forms/Hidden.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Forms" package.
  *
- * Copyright (C) 2012,2013 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -11,7 +11,6 @@
  * @subpackage  Forms
  * @author      Daniel Ménard <daniel.menard@laposte.net>
  */
-
 namespace Docalist\Forms;
 
 /**
@@ -21,9 +20,12 @@ namespace Docalist\Forms;
  * {@link http://www.w3.org/TR/html5/forms.html#hidden-state-(type=hidden) input
  * type=hidden}.
  */
-class Hidden extends Input {
-    /**
-     * @inheritdoc
-     */
-    protected $attributes = array('type' => 'hidden');
+class Hidden extends Input
+{
+    protected $attributes = ['type' => 'hidden'];
+
+    protected function hasLayout()
+    {
+        return false;
+    }
 }
