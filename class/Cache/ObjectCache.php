562d3a9e87d61b0039545a1ff2883eac4722abb6
Daniel Ménard <daniel.menard@laposte.net>
2016-01-07 09:30:51 +0100

    Amélioration du service ObjectCache
    - les performances sont vraiment mauvaises quand on n'a pas de cache du
    tout : on passe notre temps à recompiler sans cesse les même schémas.
    - modification de la classe ObjectCache pour qu'on ait toujours un cache
    - par défaut, le cache est interne et il est vidé à la fin de la
    requête, ce qui ne gène pas le développement lorsqu'on modifie
    régulièrement les schémas
    - le paramètre passé au constructeur indique maintenant si on veut
    utiliser ou non le cache WordPress (qui lui-même sera pérenne ou non
    selon qu'on a ou non un drop'in de cache).
    
    Au final, ça améliore vraiment les performances lorsqu'on est en mode
    développement. 
    
    Sur mon poste, avec php5, pour un formulaire de saisie organisme,
    j'obtiens les chiffres suivants : 
    - 4600 ms avant
    - 1600 avec la nouvelle version (cache interne)
    - 1100 avec cache WordPress + APC

--- class/Cache/ObjectCache.php
+++ class/Cache/ObjectCache.php
@@ -17,21 +17,44 @@ use stdClass;
 use WP_Admin_Bar;
 
 /**
- * Un wrapper très léger autour de l'object cache de WordPress.
+ * Le cache d'objets de Docalist.
  *
- * Par rapport au cache WordPress, ce service permet :
- * - d'activer et de désactiver facilement le cache (paramètre du constructeur)
- * - de "mocker" le cache dans les tests unitaires.
+ * Ce cache permet de stocker des objets en cache pour éviter de les construire
+ * plusieurs fois (par exemple des schémas).
+ *
+ * Par défaut, le cache est géré en interne et il n'est pas pérenne (c'est un simple
+ * tableau en mémoire qui sera "vidé" à la fin de la requête).
+ *
+ * Lors de l'appel au constructeur, il est possible d'indiquer qu'on veut utiliser
+ * le cache d'objets de WordPress. Dans ce cas, le cache sera pérenne (l'idéal) ou
+ * non selon la façon dont WordPress est paramétré : si on utilise un drop'in comme
+ * APC cache ou MemCached, le cache sera pérenne, sinon il ne le sera pas.
+ *
+ * Lorsqu'on demande à utiliser le cache de WordPress, le cache Docalist se comporte
+ * comme un wrapper très léger autour de l'object cache de WordPress.
+ *
+ * Par rapport à l'utilisation directe des fonctions wp_cache_xxx de WordPress, le
+ * cache Docalist permet :
+ * - d'activer et de désactiver facilement la persistance des données en cache
+ *   (nécessaire en production mais génant lors du développement),
+ * - de "mocker" le cache dans les tests unitaires,
  * - d'avoir des statistiques spécifiques à docalist.
  */
 class ObjectCache
 {
     /**
-     * Indique si le cache est activé.
+     * Indique si on utilise le cache wordpress ou non.
      *
      * @var bool
      */
-    protected $enabled;
+    protected $wp;
+
+    /**
+     * Les entrées du cache lorsqu'on n'utilise pas le cache WordPress.
+     *
+     * @var array
+     */
+    protected $cache;
 
     /**
      * Stats sur le cache.
@@ -42,24 +65,43 @@ class ObjectCache
 
     /**
      * Initialise le cache.
+     *
+     * @param bool $useWordPressCache Indique si on utilise le cache d'objet de WordPress ou le cache interne.
      */
-    public function __construct($enabled = true)
+    public function __construct($useWordPressCache = false)
     {
-        $this->enabled = $enabled;
+        $this->wp = (bool) $useWordPressCache;
+        !$useWordPressCache && $this->cache = [];
         $this->stats = ['hits' => 0, 'misses' => 0];
         $this->debugBar();
     }
 
     /**
+     * Indique si on utilise le cache d'objet de WordPress ou le cache interne.
+     *
+     * @return bool
+     */
+    public function useWordPressCache()
+    {
+        return $this->wp;
+    }
+
+    /**
      * Récupère un objet en cache.
      *
-     * @param string $key La clé du schéma.
+     * @param string $key   La clé indiquée pour l'objet lorsqu'il a été mis en cache.
+     * @param string $group Le group indiqué pour l'objet lorsqu'il a été mis en cache.
      *
-     * @return stdClass L'objet en cache ou false si l'objet n'ets pas/plus dans le cache.
+     * @return stdClass L'objet en cache ou false si l'objet n'est pas/plus dans le cache.
      */
     public function get($key, $group = 'docalist')
     {
-        $object = $this->enabled ? wp_cache_get($key, $group) : false;
+        if ($this->wp) {
+            $object = wp_cache_get($key, $group);
+        } else {
+            $object = isset($this->cache[$group][$key]) ? $this->cache[$group][$key] : false;
+        }
+
         ++$this->stats[$object ? 'hits' : 'misses'];
 
         return $object;
@@ -68,7 +110,8 @@ class ObjectCache
     /**
      * Stocke un objet dans le cache.
      *
-     * @param string $key La clé du schéma
+     * @param string $key   Une clé unique qui sera utilisée pour stocker l'objet dans le cache.
+     * @param string $group Un identifiant permettant de regrouper ensemble les objets similaires.
      *
      * @param Object $object
      *
@@ -76,13 +119,17 @@ class ObjectCache
      */
     public function set($key, $object, $group = 'docalist')
     {
-        $this->enabled && wp_cache_set($key, $object, $group);
+        if ($this->wp) {
+            wp_cache_set($key, $object, $group);
+        } else {
+            $this->cache[$group][$key] = $object;
+        }
 
         return $this;
     }
 
     /**
-     * Retourne des stats sur le cache.
+     * Retourne des statistiques sur l'utilisation du cache.
      *
      * @return integer[] Un tableau contenant les entrées 'hits' et 'misses'.
      */
