6b5d9499815562e980b72505473d2e878975ec79
Daniel Ménard <daniel.menard@laposte.net>
2016-03-03 10:20:46 +0100

    Réintroduit temporairement la classe relation (migration des types)

---
+++ class/Type/Relation.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012-2015 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Type;
+
+use Docalist\Type\Integer;
+
+/**
+ * CETTE CLASSE N'EST PLUS UTILISEE
+ *
+ * A SUPPRIMER UNE FOIS QUE LE SITE AURA ETE CONVERTIT
+ */
+class Relation extends Integer
+{
+}
