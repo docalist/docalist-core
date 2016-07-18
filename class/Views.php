081a66ed9cdf2b680bb9a357c7f133d4264cf835
Daniel Ménard <daniel.menard@laposte.net>
2016-07-18 11:39:44 +0200

    Views : renomme path() en getPath()
    + code format + année copyright.

--- class/Views.php
+++ class/Views.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the 'Docalist Core' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2016 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -19,14 +19,12 @@ use Exception;
 /**
  * Gestionnaire de vues Docalist.
  *
- * Les vues permettent de séparer (design pattern SOC) la préparation des
- * données (contrôleur) de leur représentation (vue).
+ * Les vues permettent de séparer (design pattern SOC) la préparation des données (contrôleur) de leur
+ * représentation (vue).
  *
- * Les vues sont prédéfinies par les plugins mais peuvent être surchargées
- * par le thème en cours. Pour cela, un système de nom symbolique de la
- * forme "{plugin}:{path}" est utilisé pour désigner les vues (par exemple
- * "docalist-core:table/edit" pour désigner la vue "table/edit" du plugin
- * docalist-core).
+ * Les vues sont prédéfinies par les plugins mais peuvent être surchargées par le thème en cours.
+ * Pour cela, un système de nom symbolique de la forme "{plugin}:{path}" est utilisé pour désigner les vues
+ * (par exemple "docalist-core:table/edit" pour désigner la vue "table/edit" du plugin docalist-core).
  *
  * La vue sera alors recherchée aux emplacements suivants :
  * - wp-content/themes/{theme actuel}/views/{plugin}/{path}.php
@@ -36,8 +34,8 @@ use Exception;
  * - wp-content/themes/twentythirteen/views/docalist-core/table/edit.php
  * - wp-content/plugins/docalist-core/views/table/edit.php
  *
- * Remarque : l'extension '.php' est ajoutée automatiquement au path de la vue
- * et ne doit pas être indiquée dans le nom de la vue.
+ * Remarque : l'extension '.php' est ajoutée automatiquement au path de la vue et ne doit pas être indiquée dans
+ * le nom de la vue.
  */
 class Views
 {
@@ -46,23 +44,19 @@ class Views
      *
      * @param string $view Le nom de la vue à exécuter.
      *
-     * @param array $data Un tableau contenant les données à transmettre
-     * à la vue.
+     * @param array $data Un tableau contenant les données à transmettre à la vue.
      *
-     * Chacun des éléments sera disponible dans la vue comme une variable
-     * locale :
+     * Chacun des éléments sera disponible dans la vue comme une variable locale :
      *
      *     render('docalist-core:table/edit', ['table' => 'test'])
      *     >>> /table/edit.php : echo $table;
      *
-     * Il est possible également de passer dans le tableau la variable 'this'
-     * avec une instance d'un objet existant. Dans ce cas, la vue aura accès
-     * à $this et sera exécutée comme s'il s'agissait d'une méthode de l'objet
-     * (elle a accès à toutes les propriétés et méthodes de l'objet, y compris
-     * celles qui sont protected ou private).
+     * Il est possible également de passer dans le tableau la variable 'this' avec une instance d'un objet existant.
+     * Dans ce cas, la vue aura accès à $this et sera exécutée comme s'il s'agissait d'une méthode de l'objet
+     * (elle a accès à toutes les propriétés et méthodes de l'objet, y compris celles qui sont protected ou private).
      *
-     * La variable $view est une variable réservée. A l'intérieur d'une vue,
-     * c'est un tableau qui contient les paramètres d'exécution de la vue :
+     * La variable $view est une variable réservée. A l'intérieur d'une vue, c'est un tableau qui contient les
+     * paramètres d'exécution de la vue :
      *
      * Par exemple :
      * array(
@@ -71,20 +65,18 @@ class Views
      *     'data' => ['table' => 'test'], // les données initiales de la vue
      * )
      *
-     * Dans une vue, $view est pratique pour le débogage mais aussi pour
-     * transmettre les données à une autre vue :
+     * Dans une vue, $view est pratique pour le débogage mais aussi pour transmettre les données à une autre vue :
      *
      * docalist('views')->display('autre:vue', $view['data']);
      *
-     * @return mixed La méthode retourne ce que retourne la vue (rien en
-     * général).
+     * @return mixed La méthode retourne ce que retourne la vue (rien en général).
      *
      * @throws Exception si la vue n'existe pas.
      */
     public function display($view, array $data = [])
     {
         // Détermine le path de la vue
-        if (false === $path = $this->path($view)) {
+        if (false === $path = $this->getPath($view)) {
             $msg = __('Vue non trouvée "%s"', 'docalist-core');
             throw new Exception(sprintf($msg, $view));
         }
@@ -111,11 +103,9 @@ class Views
      *
      * @param string $view Le nom de la vue à exécuter.
      *
-     * @param array $data Un tableau contenant les données à transmettre
-     * à la vue (cf. display()).
+     * @param array $data Un tableau contenant les données à transmettre à la vue (cf. display()).
      *
-     * @return mixed La méthode retourne ce que retourne la vue (rien en
-     * général).
+     * @return mixed La méthode retourne ce que retourne la vue (rien en général).
      *
      * @throws Exception si la vue n'existe pas.
      */
@@ -130,16 +120,15 @@ class Views
     /**
      * Retourne le path de la vue ont le nom symbolique est passé en paramètre.
      *
-     * La vue est d'abord recherchée dans le répertoire "/views" du thème en
-     * cours, ce qui permet de surcharger les vues par défaut fournies par les
-     * plugins en fournissant une version spécialisée, puis dans le répertoire
+     * La vue est d'abord recherchée dans le répertoire "/views" du thème en cours, ce qui permet de surcharger les
+     * vues par défaut fournies par les plugins en fournissant une version spécialisée, puis dans le répertoire
      * "views" du plugin.
      *
      * @param string $view Nom symbolique de la vue recherchée.
      *
      * @return string|false Le path de la vue ou false si la vue n'existe pas.
      */
-    public function path($view)
+    public function getPath($view)
     {
         static $themeDir = null;
 
@@ -181,6 +170,6 @@ class Views
      */
     public function has($view)
     {
-        return false !== $this->path($view);
+        return false !== $this->getPath($view);
     }
 }
