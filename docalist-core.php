5d39549ff40fdf5800dd1095bf4dc33bc2d77b58
Daniel Ménard <daniel.menard@laposte.net>
2017-07-04 10:32:16 +0200

    docalist-core : bump version (0.14)
    La version 0.13 (créée manuellement sur github) correspond à l'état du
    plugin au 11/04/2017.

--- docalist-core.php
+++ docalist-core.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Core" plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -10,7 +10,7 @@
  * Plugin Name: Docalist Core
  * Plugin URI:  http://docalist.org
  * Description: Docalist: socle de base.
- * Version:     0.12.0
+ * Version:     0.14.0
  * Author:      Daniel Ménard
  * Author URI:  http://docalist.org/
  * Text Domain: docalist-core
