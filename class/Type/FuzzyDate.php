43f7071742b7a56b9a208efa238d8274f923dbba
Daniel Ménard <daniel.menard@laposte.net>
2017-07-04 10:36:31 +0200

    FuzzyDate : ajoute un pattern de validation à l'éditeur
    - getEditForm() ajoute maintenant un attribut "pattern" à l'éditeur pour
    valider la syntaxe de la date saisie (AAAA, AAAAMM ou AAAAMMJJ).
    - Nouvelle méthode getValidationPattern() qui construit l'expression
    régulière utilisée.
    - à virer, en commentaire : méthode test du pattern (à transférer dans
    tests unitaires).

--- class/Type/FuzzyDate.php
+++ class/Type/FuzzyDate.php
@@ -104,4 +104,147 @@ class FuzzyDate extends Text
             'd' => isset($parts[2]) ? $parts[2] : '??',
         ]);
     }
+
+    public function getEditorForm($options = null)
+    {
+        $editor = parent::getEditorForm($options);
+
+        $editor->setAttribute('pattern', $this->getValidationPattern());
+        //$editor->setAttribute('placeholder', 'AAAAMMJJ, AAAAMM ou AAAA');
+
+        return $editor;
+    }
+
+    /**
+     * Construit une expression régulière permettant de valider une fuzzy date.
+     *
+     * L'expression régulière générée accepte des dates au format AAAA, AAAAMM ou AAAAMMJJ qui respectent les
+     * contraintes suivantes :
+     *
+     * - l'année doit être comprise entre 1900 et 2099,
+     * - le mois de février peut avoir maximum 29 jours (on ne gère pas les années bisextiles),
+     * - les mois d'avril, juin, septembre et novembre peuvet avoir maximum 30 jours,
+     * - les autres mois peuvent avoir 31 jours.
+     *
+     * Remarque : le pattern généré n'a pas de délimiteurs de début et de fin (/xxx/) ni d'ancres ('^' et '$').
+     *
+     * @param string $separator Par défaut, les dates reconnues n'ont aucun séparateur entre l'année, le mois et le
+     * jour. Pour valider des dates contenant un séparateur, vous pouvez indiquer un caractère unique (par exemple '-'
+     * pour des dates au format AAAA-MM-JJ) ou un pattern indiquant les séparateurs autorisés (par exemple '[-/]'
+     * pour accepter des dates au format AAAA-MM-JJ ou AAAA/MM/JJ).
+     *
+     * @return string L'expression régulière générée.
+     */
+    protected function getValidationPattern($separator = '')
+    {
+        $monthday = $day = function($pattern) use($separator) {
+            return $separator ? "(?:$separator(?:$pattern))?" : "(?:$pattern)?";
+        };
+
+        return '' .
+            // Année entre 1900 et 2099
+            '(?:19|20)\d{2}' .
+
+            // Mois et jour optionnels
+            $monthday(
+                '(?:0[13578]|1[02])'    . $day('0[1-9]|[12][0-9]|3[01]') .  // Mois de 31 jours
+                '|(?:0[469]|11)'        . $day('0[1-9]|[12][0-9]|30') .     // Mois de 30 jours
+                '|02'                   . $day('0[1-9]|[12][0-9]')          // Mois de février : maxi 29 jours
+            );
+    }
+/*
+    // Test de la méthode getValidationPattern(), à transférer dans test phpunit
+    public function test(array $tests = null)
+    {
+        is_null($tests) && $tests = [
+            '1900',
+            '1999',
+            '2000',
+            '2099',
+            '2017',
+            '2017-01',
+            '2017-01-01',
+            '2017-01-29',
+            '2017-01-30',
+            '2017-01-31',
+            '2017-02',
+            '2017-02-01',
+            '2017-02-29',
+            '2017-03',
+            '2017-03-01',
+            '2017-03-29',
+            '2017-03-30',
+            '2017-03-31',
+            '2017-04',
+            '2017-04-01',
+            '2017-04-29',
+            '2017-04-30',
+            '2017-05',
+            '2017-05-01',
+            '2017-05-29',
+            '2017-05-30',
+            '2017-05-31',
+            '2017-06',
+            '2017-05-01',
+            '2017-06-29',
+            '2017-06-30',
+            '2017-07',
+            '2017-07-01',
+            '2017-07-29',
+            '2017-07-30',
+            '2017-07-31',
+            '2017-08',
+            '2017-08-01',
+            '2017-08-29',
+            '2017-08-30',
+            '2017-08-31',
+            '2017-09',
+            '2017-09-01',
+            '2017-09-29',
+            '2017-09-30',
+            '2017-10',
+            '2017-10-01',
+            '2017-10-29',
+            '2017-10-30',
+            '2017-10-31',
+            '2017-11',
+            '2017-11-01',
+            '2017-11-29',
+            '2017-11-30',
+            '2017-12',
+            '2017-12-01',
+            '2017-12-29',
+            '2017-12-30',
+            '2017-12-31',
+
+            '1860',
+            '2017-',
+            '2017-00',
+            '2017-1',
+            '2017-01-00',
+            '2017-02-30',
+            '2017-02-31',
+            '2017-04-31',
+            '2017-06-31',
+            '2017-09-31',
+            '2017-11-31',
+            '2017-13',
+            '2017-13-01',
+            '2100',
+        ];
+
+        $sep = '';
+
+        $re = '~^' . $this->getValidationPattern($sep) . '$~';
+        $matches = null;
+        echo '<pre>';
+        echo $re, '<br />';
+        foreach ($tests as $test) {
+            $test = str_replace('-', $sep, $test);
+            $ok = preg_match($re, $test, $matches);
+//            echo $test, ' ', ($ok ? 'ok' : 'erreur'), var_export($matches, true), '<br />';
+            echo ($ok ? 'ok...: ' : 'ERR..: '), $test, '<br />';
+        }
+    }
+*/
 }
