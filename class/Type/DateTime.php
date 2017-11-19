8f9592a0230a682a0c7c02f65c315ee88a43733a
Daniel Ménard <daniel.menard@laposte.net>
2017-11-19 11:21:51 +0100

    Type/DateTime : implémente getAvailableFormats() et getFormattedValue()
    On a maintenant plusieurs formats d'affichage proposés (d/m/Y, d-m-Y
    etc.)

--- class/Type/DateTime.php
+++ class/Type/DateTime.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of a "Docalist Core" plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -15,6 +15,8 @@
 namespace Docalist\Type;
 
 use Docalist\Forms\Input;
+use DateTime as PhpDateTime;
+use Exception;
 
 /**
  * Une date/heure stockée sous forme de chaine au format 'yyyy-MM-dd HH:mm:ss'.
@@ -52,5 +54,58 @@ class DateTime extends Text
             ->setLabel($this->getOption('label', $options))
             ->setDescription($this->getOption('description', $options))
             ->setAttribute('type', $type);
+    }
+
+    public function getAvailableFormats()
+    {
+        // Formats dispos
+        $formats = [
+            'd/m/Y',    // 04/09/2017
+            'd-m-Y',    // 04/09/2017
+
+            'd/m/y',    // 04/09/17
+            'd-m-y',    // 04/09/17
+
+            'Y-m-d',    // 2017-09-04
+
+            'j M Y',    // 4 sep 2017
+            'j F Y',    // 4 septembre 2017
+        ];
+
+        // Au lieu d'afficher un libellé, on affiche des exemples (année en cours avec un jour et un mois < 10)
+        $date = PhpDateTime::createFromFormat('d/m', '4/7'); // 4 juillet de l'année en cours
+        $examples = [];
+        foreach ($formats as $format) {
+            $examples[$format] = date_i18n($format, $date->getTimestamp());
+        }
+
+        // Ok
+        return $examples;
+    }
+
+    public function getFormattedValue($options = null)
+    {
+        $format = $this->getOption('format', $options, $this->getDefaultFormat());
+        // à gérer : si format nommé (raw, text...)
+        // utiliser date_i18n() ?
+
+        // Essaie de convertir la date au format texte en objet DateTime
+        try {
+            $dateTime = new PhpDateTime($this->phpValue);
+        }
+
+        // En cas d'erreur (syntaxe incorrecte, etc.), on retourne la valeur "brute"
+        catch (Exception $e) {
+            return $this->phpValue;
+        }
+
+        // Récupère le timestamp correspondant et demande à WordPress de formatter
+        return date_i18n($format, $dateTime->getTimestamp());
+
+        // Demande à l'objet DateTime de formatter la date avec le format indiqué
+        // Pas de bloc try/catch : si le format n'est pas bon, on veut une exception
+        return $dateTime->format($format);
+        //throw new InvalidArgumentException("Invalid link format '$format'");
+        //return
     }
 }
