5208783d340e1288f2a8a73c3e38b15286f34852
Daniel Ménard <daniel.menard@laposte.net>
2018-01-24 11:57:24 +0100

    Transfert des tables pays/langues/continents dans docalist-data

--- class/Core/Installer.php
+++ class/Core/Installer.php
@@ -9,9 +9,6 @@
  */
 namespace Docalist\Core;
 
-use Docalist\Table\TableManager;
-use Docalist\Table\TableInfo;
-
 /**
  * Installation/désinstallation de docalist-core.
  *
@@ -24,138 +21,19 @@ class Installer
      */
     public function __construct()
     {
-        // Charge le plugin docalist-core si ce n'est pas encore fait : quand wp
-        // exécute "plugin_sandbox_scrape", plugins_loaded n'a pas encore été appellé
-        if (! docalist('services')->has('docalist-core')) {
-            docalist('services')->add('docalist-core', new Plugin());
-        }
     }
 
     /**
-     * Activation : enregistre les tables prédéfinies.
+     * Activation.
      */
     public function activate()
     {
-        $tableManager = docalist('table-manager'); /** @var TableManager $tableManager */
-
-        // Enregistre les tables prédéfinies
-        foreach ($this->getTables() as $name => $table) {
-            $table['name'] = $name;
-            $table['lastupdate'] = date_i18n('Y-m-d H:i:s', filemtime($table['path']));
-            $tableManager->register(new TableInfo($table));
-        }
     }
 
     /**
-     * Désactivation : supprime les tables prédéfinies.
+     * Désactivation.
      */
     public function deactivate()
     {
-        $tableManager = docalist('table-manager'); /** @var TableManager $tableManager */
-
-        // Supprime les tables prédéfinies
-        foreach (array_keys($this->getTables()) as $table) {
-            $tableManager->unregister($table);
-        }
-    }
-
-
-    /**
-     * Retourne la liste des tables prédéfinies.
-     *
-     * @return array
-     */
-    protected function getTables()
-    {
-        $dir = DOCALIST_CORE_DIR . DIRECTORY_SEPARATOR . 'tables'  . DIRECTORY_SEPARATOR;
-
-        return [
-            // Tables des langues complète
-            'ISO-639-2_alpha3_fr' => [
-                'path' => $dir . 'languages/ISO-639-2_alpha3_fr.txt',
-                'label' => __('Langues (français)', 'docalist-core'),
-                'format' => 'table',
-                'type' => 'languages',
-                'creation' => '2014-03-14 10:11:23',
-            ],
-            'ISO-639-2_alpha3_en' => [
-                'path' => $dir . 'languages/ISO-639-2_alpha3_en.txt',
-                'label' => __('Langues (anglais)', 'docalist-core'),
-                'format' => 'table',
-                'type' => 'languages',
-                'creation' => '2014-03-14 10:11:43',
-            ],
-
-            // Tables des langues simplifiées (langues officielles de l'union européenne)
-            'ISO-639-2_alpha3_EU_fr' => [
-                'path' => $dir . 'languages/ISO-639-2_alpha3_EU_fr.txt',
-                'label' => __('Langues UE (français)', 'docalist-core'),
-                'format' => 'table',
-                'type' => 'languages',
-                'creation' => '2014-03-15 09:01:39',
-            ],
-            'ISO-639-2_alpha3_EU_en' => [
-                'path' => $dir . 'languages/ISO-639-2_alpha3_EU_en.txt',
-                'label' => __('Langues UE (anglais)', 'docalist-core'),
-                'format' => 'table',
-                'type' => 'languages',
-                'creation' => '2014-03-15 09:01:39',
-            ],
-
-            // Tables de conversion des codes langues
-            'ISO-639-2_alpha2-to-alpha3' => [
-                'path' => $dir . 'languages/ISO-639-2_alpha2-to-alpha3.txt',
-                'label' => __('Langues - conversion alpha2 <-> alpha3', 'docalist-core'),
-                'format' => 'conversion',
-                'type' => 'languages',
-                'creation' => '2014-03-14 10:12:15',
-            ],
-
-            // Tables des pays
-            'ISO-3166-1_alpha2_fr' => [
-                'path' => $dir . 'countries/ISO-3166-1_alpha2_fr.txt',
-                'label' => __('Pays (français)', 'docalist-core'),
-                'format' => 'table',
-                'type' => 'countries',
-                'creation' => '2014-03-14 10:08:17',
-            ],
-            'ISO-3166-1_alpha2_EN' => [
-                'path' => $dir . 'countries/ISO-3166-1_alpha2_en.txt',
-                'label' => __('Pays (anglais)', 'docalist-core'),
-                'format' => 'table',
-                'type' => 'countries',
-                'creation' => '2014-03-14 10:08:32',
-            ],
-            'ISO-3166-1_alpha3-to-alpha2' => [
-                'path' => $dir . 'countries/ISO-3166-1_alpha3-to-alpha2.txt',
-                'label' => __('Langues - conversion alpha3 <-> alpha2', 'docalist-core'),
-                'format' => 'conversion',
-                'type' => 'countries',
-                'creation' => '2014-03-14 10:09:01',
-            ],
-
-            // Tables des continents
-            'continents_fr' => [
-                'path' => $dir . 'continents/continents_fr.txt',
-                'label' => __('Continents (français)', 'docalist-core'),
-                'format' => 'table',
-                'type' => 'continents',
-                'creation' => '2016-12-11 10:17:48',
-            ],
-            'continents_en' => [
-                'path' => $dir . 'continents/continents_en.txt',
-                'label' => __('Continents (anglais)', 'docalist-core'),
-                'format' => 'table',
-                'type' => 'continents',
-                'creation' => '2016-12-11 10:18:03',
-            ],
-            'country-to-continent' => [
-                'path' => $dir . 'countries/country-to-continent.txt',
-                'label' => __('Continents - conversion pays <-> continent', 'docalist-core'),
-                'format' => 'conversion',
-                'type' => 'countries',
-                'creation' => '2016-12-11 10:18:03',
-            ],
-        ];
     }
 }
