9a98af63d335f77fe9a0bb0286a3d201cae6b450
Daniel Ménard <daniel.menard@laposte.net>
2018-10-18 12:13:48 +0200

    Vue "outil disponible".

---
+++ class/Tools/views/available-tool.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of Docalist Core.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\Tools\Views;
+
+use Docalist\AdminPage;
+
+/**
+ * Affiche une "carte" présentant les outils Docalist dans la page WordPress "Outils disponibles".
+ */
+
+/* @var AdminPage $this */  // La page parent
+?>
+<div class="card">
+    <h2 class="title"><?= $this->menuTitle() ?></h2>
+    <p>
+        <?= __(
+            'Permet de lancer des scripts et des traitements longs liés à Docalist
+            (migration de données, corrections, opérations de maintenance...)',
+            'docalist-core'
+        ); ?>
+
+        <a href="<?= esc_attr($this->getUrl()) ?>">
+            <?= __('Voir la liste des outils Docalist.', 'docalist-core') ?>
+        </a>
+    </p>
+</div>
