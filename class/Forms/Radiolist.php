70a082da1910830ab9dd03d256c6c1cec625c945
Daniel Ménard <daniel.menard@laposte.net>
2018-11-19 10:56:45 +0100

    RadioList : adaptation nouveau modèle "Choice"
    - plus besoin de surcharger displayOptions()
    - on se contente d'implémenter displayOption(), startOptionGroup() et
    endOptionGroup()
    - view/radiolist : simplification
    - strict_types
    - types hints
    - {@inheritdoc}

--- class/Forms/Radiolist.php
+++ class/Forms/Radiolist.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * This file is part of Docalist Core.
  *
@@ -17,11 +17,16 @@ namespace Docalist\Forms;
 class Radiolist extends Choice
 {
     /**
-     * La valeur de l'attribut 'type' du contrôle input généré ('radio' ou 'checkbox').
+     * La valeur de l'attribut 'type' dans le tag input généré par displayOption().
      *
      * @var string
      */
     const INPUT_TYPE = 'radio';
+
+    /**
+     * {@inheritdoc}
+     */
+    const CSS_CLASS = 'radiolist';
 
     /**
      * Une checklist ou une radiolist sont représentés par des <ul>, le libellé associé ne doit pas
@@ -29,44 +34,67 @@ class Radiolist extends Choice
      *
      * @return boolean
      */
-    protected function isLabelable()
+    protected function isLabelable(): bool
     {
         return false;
     }
 
-    protected function displayOptions(Theme $theme, array $options = [], array $data = [], array $attributes = [])
+    /**
+     * {@inheritdoc}
+     */
+    protected function displayOption(Theme $theme, string $value, string $label, bool $selected, bool $invalid): void
     {
-        $visited = [];
-        foreach ($options as $value => $label) {
-            // Si label est un tableau c'est un optgroup : value contient le libellé du optgroup et label les options
-            if (is_array($label)) {
-                $theme
-                    ->start('li', ['class' => 'optgroup'])
-                    ->tag('p', ['class' => 'optgroup-label'], sprintf(__('%s :', 'docalist-core'), $value))
-                    ->start('ul');
-                $this->displayOptions($theme, $label, $data);
-                foreach ($label as $value => $label) {
-                    $visited[$value] = $value;
-                }
-                $theme->end('ul')->end('li');
-                continue;
-            }
-
-            // Option normale
-            $attr = ['name' => $this->getControlName(), 'type' => static::INPUT_TYPE, 'value' => $value];
-            if (in_array($value, $data, false)) {
-                $attr['checked'] = 'checked';
-                $visited[$value] = $value;
-            }
-
-            $theme
-                ->start('li')
-                    ->start('label', $attributes)
-                        ->tag('input', $attr)->html($label)
-                    ->end('label')
-                ->end('li');
+        // Détermine les attributs de l'option
+        $attributes = [];
+        if ($invalid) {
+            $class = static::CSS_CLASS . '-invalid-entry';
+            $this->isRepeatable() && $class .= ' do-not-clone'; // évite de cloner les options invalides
+            $attributes['class'] = $class;
+            $attributes['title'] = __('Option invalide', 'docalist-core');
         }
 
-        return array_diff_key(array_combine($data, $data), $visited);
+        // Début du <li>
+        $theme->start('li', $attributes);
+
+        // Début du <label>
+        $theme->start('label');
+
+        // Case à cocher
+        $attributes = ['name' => $this->getControlName(), 'type' => static::INPUT_TYPE, 'value' => $value];
+        $selected && $attributes['checked'] = 'checked';
+        $theme->tag('input', $attributes)->html($label);
+
+        // Fin du <label>
+        $theme->end('label');
+
+        // Fin du <li>
+        $theme->end('li');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function startOptionGroup(string $label, Theme $theme): void
+    {
+        // Début du <li>
+        $theme->start('li', ['class' => static::CSS_CLASS . '-group']);
+
+        // Libellé du groupe
+        $theme->tag('p', ['class' => static::CSS_CLASS . '-group-label'], $label);
+
+        // Début des options du groupe
+        $theme->start('ul');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function endOptionGroup(Theme $theme): void
+    {
+        // Fin des options du groupe
+        $theme->end('ul');
+
+        // Fin du <li>
+        $theme->end('li');
     }
 }
