2a0ce73576c08c5bf6c31baf65af9b5d310a8ddf
Daniel Ménard <daniel.menard@laposte.net>
2018-11-19 10:52:03 +0100

    Choice : révision complète de la génération des options
    - Nouvelle méthode abstraite displayOption() qui se charge d'afficher
    une option
    - Nouvelles méthodes abstraites startOptionGroup() et endOptionGroup()
    pour afficher un groupe d'options
    - displayOptions() est désormais une méthode concrète qui gère toute la
    logique et appelle loadOptions, displayOptions, startOptionGroup et
    endOptionGroup.
    - loadOptions() prend maintenant en paramètre la liste des options
    sélectionnées. Ca permet à EntryPicker de ne charger que les options
    sélectionnées. On contrôle également le type des les lookups et on
    n'autorise que les lookups de type table ou thesaurus.
    - Nouvelle constante CSS_CLASS, surchargée dans les classes
    descendantes, qui indique la classe css de base du contrôle généré.
    - passe en "final" tout ce qu'on peut
    - révisions phpdoc
    - strict types
    - types hint

--- class/Forms/Choice.php
+++ class/Forms/Choice.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * This file is part of Docalist Core.
  *
@@ -9,43 +9,63 @@
  */
 namespace Docalist\Forms;
 
-use Docalist\Table\TableInterface;
+use Docalist\Table\TableManager;
+use InvalidArgumentException;
 
 /**
  * Classe de base pour les champs qui permettent à l'utilisateur de faire un choix parmi une liste de
- * valeurs possibles (select, checklist, radiolist).
+ * valeurs possibles (select, checklist, radiolist, entrypicker).
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
 abstract class Choice extends Element
 {
     /**
-     * @var array|callable|string Les options disponibles.
+     * Le préfixe des classes CSS qui sont générées par displayOption() et startOptionGroup().
+     *
+     * @var string
+     */
+    const CSS_CLASS = 'choice';
+
+    /**
+     * La liste des options disponibles.
+     *
+     * @var array|callable|string
      */
     protected $options = [];
 
     /**
      * Modifie la liste des options disponibles.
      *
-     * @param array|callable|string $options Vous pouvez indiquer :
+     * Les options disponibles peuvent être fournies au contrôle en indiquant :
      *
-     * - Un tableau de la forme 'value => label' contenant les options disponibles :
+     * - un tableau de la forme code => label,
+     * - un callable qui retourne la liste des options,
+     * - une chaine de lookup indiquant où se trouvent les options disponibles.
      *
-     *   setOptions(['fr' => 'french', 'en' => english])
+     * Exemples :
+     * <code>
+     *     setOptions(['fr' => 'french', 'en' => english]); // Appel avec un tableau d'options
+     *     setOptions(function() {                          // Appel avec un callable qui retourne les options
+     *         return ['fr' => 'french', 'en' => english];
+     *     });
+     *     setOptions('table:dates');                       // Appel avec un lookup de type table
+     *     setOptions('thesaurus:dates');                   // Appel avec un lookup de type thésaurus
+     * </code>
      *
-     * - Un callable qui retourne un tableau contenant les options :
+     * Remarque : les contrôles de base (RadioList, CheckList, Select) ne supportent que les lookups de type 'table'
+     * ou 'thesaurus'. Les lookups de type 'index' ou 'search' ne sont pas supportés car ça conduirait à injecter
+     * dans le code html de la page des listes potentiellement gigantesques. Pour des lookups de ce type, il faut
+     * utiliser le contrôle EntryPicker qui supporte les requêtes ajax et n'injecte pas la totalité des options
+     * possibles dans le code de la page.
      *
-     *   setOptions(function() { return ['fr' => 'french', 'en' => english]; });
+     * @param array|callable|string $options La liste des options disponibles.
      *
-     * - Une chaine de caractères :
-     *
-     *   - le nom d'une table ou d'un thésaurus : setOptions('countries');
-     *   - la chaine 'index' ??
-     *   - une recherche : setOptions('search:type:person %s')
+     * @throws InvalidArgumentException Si les options indiquées ne sont pas valides.
      *
      * @return self
      */
-    public function setOptions($options)
+    final public function setOptions($options): self
     {
         if (is_array($options) || is_string($options) || is_callable($options)) {
             $this->options = $options;
@@ -53,7 +73,7 @@ abstract class Choice extends Element
             return $this;
         }
 
-        return $this->invalidArgument('%s: invalid options (%s)', gettype($options));
+        $this->invalidArgument('%s: invalid options (%s)', gettype($options));
     }
 
     /**
@@ -61,59 +81,165 @@ abstract class Choice extends Element
      *
      * @return array|callable|string
      */
-    public function getOptions()
+    final public function getOptions()// : mixed
     {
         return $this->options;
     }
 
     /**
-     * Charge la liste des options disponibles.
+     * Charge la liste des options qui seront affichées dans le contrôle.
      *
      * - Si $options est un tableau, la méthode se contente de le retourner.
-     * - Si $options est une closure, la méthode exécute la closure et retourne le résultat.
-     * - Si $options est une chaine (nom de table ou de thésaurus), la méthode charge les options disponibles.
+     * - Si $options est une closure, la méthode exécute la closure et retourne le résultat (un tableau d'options).
+     * - Si $options est une chaine (lookup de type table ou thésaurus), la méthode charge les options disponibles.
      *
-     * @return array
+     * @param string[] $selected Les données actuellement sélectionnées. Ce paramètre n'est utilisé que par la classe
+     * EntryPicker qui n'affiche initialement que les options sélectionnées (les autres options disponibles sont
+     * obtenues via des requêtes ajax). Pour tous les autres types de Choice (RadioList, CheckList et Select), le
+     * paramètre $selected est ignoré.
+     *
+     * @return string[] Un tableau de la forme code => valeur.
      */
-    protected function loadOptions()
+    protected function loadOptions(array $selected = []): array
     {
+        // Si c'est un tableau, on le retourne tel quel
         if (is_array($this->options)) {
             return $this->options;
         }
 
+        // Si c'est un callback, on l'exécute
+        if (is_callable($this->options)) {
+            return ($this->options)($this);
+        }
+
+        // Si c'est une chaine de lookup, vérifie que c'est une table ou un thesaurus et retourne le contenu
         if (is_string($this->options)) {
-            list(, $name) = explode(':', $this->options);
+            // Détermine le type et la source des lookups
+            list($type, $source) = explode(':', $this->options, 2);
 
-            $table = docalist('table-manager')->get($name); /* @var TableInterface $table */
+            // Pour les Choice de base (radiolist, select...) on ne gère que les lookups de type table ou thesaurus
+            // (avec des lookups de type index ou search on chargerait des listes potentiellement énormes dans
+            // le code html de la page et en plus le format des entrées n'est plus le même dans ce cas là).
+            if (!($type === 'table' || $type === 'thesaurus')) {
+                $this->invalidArgument('Lookups of type "%s" are not supported, try with EntryPicker');
+            }
 
+            // Ouvre la table
+            $tableManager = docalist('table-manager'); /** @var TableManager $tableManager */
+            $table = $tableManager->get($source);
+
+            // Charge la totalité des entrées de la table et retourne un tableau de la forme code => valeur
             return $table->search('code,label');
         }
 
-        if (is_callable($this->options)) {
-            $callback = $this->options;
-
-            return $callback($this);
-        }
-
-        return $this->invalidArgument('%s: invalid options (%s)', gettype($this->options));
+        // ça ne peut pas être autre chose (cf. setOptions)
     }
 
     /**
-     * Affiche les options passées en paramètre.
+     * Affiche la liste des options disponibles.
      *
-     * Cette méthode utilitaire permet de simplifier les vues (exemples views/form/base/select.php ou checklist.php).
+     * La méthode affiche toutes les options qui sont retournées par loadOptions($selected). En général, il s'agit
+     * de la totalité des options disponibles, telles qu'indiquées dans la source (setOptions). Pour le contrôle
+     * EntryPicker, seules les options actuellement sélectionnées sont affichées (les autres options disponibles
+     * sont chargées via des requêtes ajax).
      *
-     * @param Theme $theme      Le thème à utiliser.
-     * @param array $options    Les options à afficher.
-     * @param array $data       Les options actuellement sélectionnées (données).
-     * @param array $attributes Les attributs à ajouter à chacune des options.
+     * Les méthode gère la logique de la génération mais ce sont les méthodes displayOption(), startOptionGroup()
+     * et endOptionGroup() qui génèrent les tags qui seront affichés. Ces trois méthodes doivent être implémentées
+     * dans les classes descendantes.
      *
-     * @return array Les valeurs invalides (celles qui figurent dans les données mais pas dans les options).
+     * @param Theme     $theme      Thème à utiliser.
+     * @param string[]  $selected   Les données actuelles (i.e. les options qui auront l'attribut "selected").
      */
-    abstract protected function displayOptions(
-        Theme $theme,
-        array $options = [],
-        array $data = [],
-        array $attributes = []
-    );
+    protected function displayOptions(Theme $theme, array $selected): void
+    {
+        // Charge les options disponibles (on passe $selected car EntryPicker ne charge que les options sélectionnées)
+        $options = $this->loadOptions($selected);
+
+        // Indexe les données par clé : au fur et à mesure de l'affichage, on les supprimera du tableau et si à la
+        // fin il reste encore des données, elles seront affichées comme options invalides
+        $selected = array_combine($selected, $selected);
+
+        // Affiche toutes les options
+        foreach ($options as $value => $label) {
+            // Si label est une chaine, c'est une option simple
+            if (is_string($label)) {
+                // Affiche l'option
+                $this->displayOption($theme, $value, $label, isset($selected[$value]), false);
+
+                // Supprime l'option de la liste des options sélectionnées
+                unset($selected[$value]);
+
+                // Passe à l'option suivante
+                continue;
+            }
+
+            // Groupe d'options : value contient le label, et label doit contenur un tableau d'options
+            if (! is_array($label)) {
+                $this->invalidArgument('%s: invalid options for optgroup "%s", expected array.', $value);
+            }
+
+            // Génère le début du groupe
+            $this->startOptionGroup($value, $theme);
+
+            // Génère les options du groupe
+            foreach ($label as $value => $label) {
+                // Si label est une chaine, c'est une option simple
+                if (is_string($label)) {
+                    // Affiche l'option
+                    $this->displayOption($theme, $value, $label, isset($selected[$value]), false);
+
+                    // Supprime l'option de la liste des options sélectionnées
+                    unset($selected[$value]);
+
+                    // Passe à l'option suivante
+                    continue;
+                }
+
+                // Groupe d'options imbriqué, ce n'est pas autorisé, génère une exception
+                if (is_array($label)) {
+                    $this->invalidArgument('%s: invalid option "%s", options groups cannot be nested.', $value);
+                }
+            }
+
+            // Génère la fin du groupe
+            $this->endOptionGroup($theme);
+        }
+
+        // S'il reste encore des données dans selected, ce sont des options invalides
+        foreach ($selected as $value) {
+            $this->displayOption($theme, $value, $value, true, true);
+        }
+    }
+
+    /**
+     * Affiche une option.
+     *
+     * @param Theme     $theme      Thème à utiliser.
+     * @param string    $value      Code de l'option.
+     * @param string    $label      Libellé de l'option.
+     * @param bool      $selected   True si l'option est sélectionnée (i.e. si elle figure dans le champ).
+     * @param bool      $invalid    True si l'option figure dans le champ mais pas dans les options disponibles.
+     */
+    abstract protected function displayOption(
+        Theme   $theme,
+        string  $value,
+        string  $label,
+        bool    $selected,
+        bool    $invalid
+    ): void;
+
+    /**
+     * Affiche le tag de début d'un groupe d'options.
+     *
+     * @param string    $label      Libellé du groupe d'options.
+     * @param Theme     $theme      Thème à utiliser.
+     */
+    abstract protected function startOptionGroup(string $label, Theme $theme): void;
+
+    /**
+     * Affiche le tag de fin d'un groupe d'options.
+     *
+     * @param Theme     $theme      Thème à utiliser.
+     */
+    abstract protected function endOptionGroup(Theme $theme): void;
 }
