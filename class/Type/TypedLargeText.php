c7e6ba3575be44ddbbbf303bec847a5fb8fe5760
Daniel Ménard <daniel.menard@laposte.net>
2019-03-07 11:31:47 +0100

    TypedLargeText : tweaks doc

--- class/Type/TypedLargeText.php
+++ class/Type/TypedLargeText.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of Docalist Core.
  *
- * Copyright (C) 2012-2018 Daniel Ménard
+ * Copyright (C) 2012-2019 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
@@ -10,15 +10,12 @@
 namespace Docalist\Type;
 
 use Docalist\Type\TypedText;
-use Docalist\Type\TableEntry;
 use Docalist\Type\LargeText;
 
 /**
- * Texte large typé : un type composite associant un type provenant d'une table d'autorité à une valeur de
- * type LargeText.
+ * TypedLargeText : un TypedText qui a une valeur de type LargeText.
  *
- * @property TableEntry $type   Type de texte.
- * @property LargeText  $value  Texte associé.
+ * @property LargeText $value Texte associé.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
