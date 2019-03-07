bd48eaa00403ea5f888ed87ddefa8d0b302bae79
Daniel Ménard <daniel.menard@laposte.net>
2019-03-07 11:30:58 +0100

    TypedFuzzyDate : hérite de TypedValue

--- class/Type/TypedFuzzyDate.php
+++ class/Type/TypedFuzzyDate.php
@@ -2,26 +2,24 @@
 /**
  * This file is part of Docalist Core.
  *
- * Copyright (C) 2012-2018 Daniel Ménard
+ * Copyright (C) 2012-2019 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
  */
 namespace Docalist\Type;
 
-use Docalist\Type\TypedText;
-use Docalist\Type\TableEntry;
+use Docalist\Type\TypedValue;
 use Docalist\Type\FuzzyDate;
 
 /**
- * Date typée : un type composite associant un champ TableEntry à une valeur de type FuzzyDate.
+ * TypedFuzzyDate : un TypedValue qui a une valeur de type FuzzyDate.
  *
- * @property TableEntry $type   Type de date.
- * @property FuzzyDate  $value  Date associée.
+ * @property FuzzyDate $value Date associée.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-class TypedFuzzyDate extends TypedText
+class TypedFuzzyDate extends TypedValue
 {
     public static function loadSchema()
     {
