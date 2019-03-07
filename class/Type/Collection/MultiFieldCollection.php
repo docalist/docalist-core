03418420125a5165e4809d769bc3634e79e47b2b
Daniel Ménard <daniel.menard@laposte.net>
2019-03-07 10:31:48 +0100

    Nouvelle classe MultiFieldCollection : une collection de MultiField
    - surcharge filterItems() pour filtre sur le <type> des éléments (par
    défaut dans Collection on filtre par valeur)
    - gère un filtre interne qui peut être activé ou désactivé et qui permet
    de retourner ou non les éléments qui ont un type de la forme "internal*"

---
+++ class/Type/Collection/MultiFieldCollection.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * This file is part of Docalist Core.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\Type\Collection;
+
+use Docalist\Type\Collection;
+use Docalist\Type\MultiField;
+use Docalist\Type\Any;
+
+/**
+ * Une collection d'objets MultiField.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class MultiFieldCollection extends Collection
+{
+    /**
+     * Indique si le filtre sur les éléments "internal*" est activé ou non (true par défaut).
+     *
+     * @var bool
+     */
+    private static $internalFilter = true;
+
+    /**
+     * Indique si le filtre sur les éléments "internal*" est activé ou non.
+     *
+     * @return bool
+     */
+    public function internalFilterIsEnabled(): bool
+    {
+        return self::$internalFilter;
+    }
+
+    /**
+     * Active le filtre sur les éléments "internal*".
+     *
+     * Lorsque le filtre est activé, les méthodes filter() et similaires filtrent les éléments qui ont un
+     * type de la forme "internal*".
+     */
+    public static function enableInternalFilter(): void
+    {
+        self::$internalFilter = true;
+    }
+
+    /**
+     * Désactive le filtre sur les éléments "internal*".
+     *
+     * Lorsque le filtre est désactivé, les méthodes filter() et similaires ne filtrent pas les éléments qui ont un
+     * type de la forme "internal*".
+     */
+    public function disableInternalFilter(): void
+    {
+        self::$internalFilter = false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function filterItem(Any $item, array $include = [], array $exclude = []): ?Any
+    {
+        /** @var MultiField $item */
+
+        // On filtre par catégorie
+        $value = $item->getCategoryCode();
+
+        // Si on a une liste blanche et que l'item n'y figure pas, on l'ignore
+        if ($include && !in_array($value, $include, true)) {
+            return null;
+        }
+
+        // Si on a une liste noire et que l'item y figure, on l'ignore
+        if ($exclude && in_array($value, $exclude, true)) {
+            return null;
+        }
+
+        // Si c'est un item "internal" et que le filtre est activé, on l'ignore
+        if (self::$internalFilter && strncmp($value, 'internal', 8) === 0) {
+            return null;
+        }
+
+        // Ok
+        return $item;
+    }
+}
