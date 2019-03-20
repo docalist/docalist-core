12d0b0b445ff73c7f24195e1367e758040b5eafd
Daniel Ménard <daniel.menard@laposte.net>
2019-03-20 16:37:11 +0100

    Text::getEditorForm() : caste les options
    évite des warnings dans scrutinizer

--- class/Type/Text.php
+++ class/Type/Text.php
@@ -52,7 +52,7 @@ class Text extends Scalar
 
     public function getEditorForm($options = null): Element
     {
-        $editor = $this->getOption('editor', $options, $this->getDefaultEditor());
+        $editor = (string) $this->getOption('editor', $options, $this->getDefaultEditor());
         $css = '';
         switch ($editor) {
             case 'input':
