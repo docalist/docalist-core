93540f5228690c7c2c5131338dd10ceb35b9eca2
Daniel Ménard <daniel.menard@laposte.net>
2020-02-21 17:39:40 +0100

    LargeText : filtres WordPress (embed, shortcodes, smileys...)

--- class/Type/LargeText.php
+++ class/Type/LargeText.php
@@ -15,6 +15,8 @@ use Docalist\Forms\Element;
 use Docalist\Forms\Textarea;
 use Docalist\Forms\WPEditor;
 use Docalist\Forms\CodeEditor;
+use Docalist\Forms\Container;
+use WP_Embed;
 
 /**
  * Un bloc de texte multiligne contenant ou non du code html.
@@ -23,6 +25,9 @@ use Docalist\Forms\CodeEditor;
  */
 class LargeText extends Text
 {
+    /**
+     * {@inheritDoc}
+     */
     public function getAvailableEditors(): array
     {
         return [
@@ -34,6 +39,9 @@ class LargeText extends Text
         ];
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getEditorForm($options = null): Element
     {
         $editor = (string) $this->getOption('editor', $options, $this->getDefaultEditor());
@@ -80,28 +88,96 @@ class LargeText extends Text
         return $this->configureEditorForm($form, $options);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function getFormatSettingsForm(): Container
+    {
+        $form = parent::getFormatSettingsForm();
+        $form->checklist('filters')
+            ->setLabel(__('Filtres WordPress', 'docalist-core'))
+            ->setDescription(__(
+                "Par défaut, le contenu du champ est affiché tel quel. Vous pouvez activer les options
+                proposées si vous voulez que votre champ se comporte comme un article WordPress standard.",
+                'docalist-core'
+            ))
+            ->setOptions([
+                'autop' => 'Paragraphes auto (crée des paragraphes html quand on a deux retours à la ligne)',
+                'texturize' => 'Texturize (version typographique des apostrophes, guillemets, tirets, ellipses...)',
+                'smilies' => 'Smileys (convertit les émoticônes comme :-) en smileys)',
+                'autoembed' => 'Auto-embed (intègre les médias liés dans le contenu)',
+                'shortcodes' => 'Shortcodes (exécute les shortcodes présents dans le contenu)',
+                'responsive-images' => 'Formats multiples pour les images (pour les rendre responsive)',
+            ]);
+
+        return $form;
+    }
+
+    /**
+     * Retourne l'objet WP_Embed de WordPress (pour éviter de coder des "global" partout).
+     *
+     * @return WP_Embed
+     */
+    private function wpEmbed(): WP_Embed
+    {
+        return $GLOBALS['wp_embed'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function getFormattedValue($options = null)
     {
-        $value = $this->phpValue;
+        $value = $this->getPhpValue();
         if (trim($value) === '') {
             return $value;
         }
-        true && $value = wpautop($value);
-        true && $value = wp_make_content_images_responsive($value);
+
+        $filters = $this->getOption('filters', $options, ['autop', 'responsive-images']);
+
+        if (in_array('autoembed', $filters)) {
+            $value = $this->wpEmbed()->run_shortcode($value);
+            $value = $this->wpEmbed()->autoembed($value);
+        }
+
+        if (in_array('texturize', $filters)) {
+            $value = wptexturize($value);
+        }
+
+        if (in_array('smilies', $filters)) {
+            $value = convert_smilies($value);
+        }
+
+        if (in_array('autop', $filters)) {
+            $value = wpautop($value);
+            $value = shortcode_unautop($value);
+        }
+
+        if (in_array('responsive-images', $filters)) {
+            $value = wp_make_content_images_responsive($value);
+        }
+
+        if (in_array('shortcodes', $filters)) {
+            $value = do_shortcode($value);
+        }
 
         return $value;
 
-        // Filtres wordpress (4.4) par défaut pour "the_content" (dans default-filters.php) :
-        // Nom                                  Priorité    Rôle
+        // Filtres wordpress (5.2.5) par défaut pour "the_content" :
+        //
+        // Dans default-filters.php :
         // capital_P_dangit()                   11          Change 'wordpress' en 'wordPress'...
+        // do_blocks()                           9          Fait le rendu des blocs gutenberg
         // wptexturize()                        10          Transforme les guillemets, apostrophes, etc.
-        // convert_smilies()                    10          Convertit les smileys
+        // convert_smilies()                    20          Convertit les smileys
         // wpautop()                            10          Remplace \n\n par des tags <p>..</p>
         // shortcode_unautop()                  10          Supprime les <p></p> en trop autour des shortcodes
         // prepend_attachment()                 10          Attachments uniquement : ajoute un lien
         // wp_make_content_images_responsive()  10          Ajoute des attr 'srcsets' et 'sizes' aux images
         // do_shortcode()                       11          Exécute les shortcodes
-
-        // Voir s'il faut ou non ajouter des options à LargeText pour activer ou non ces filtres.
+        //
+        // Dans class-wp-embed.php :
+        // $wp_embed->run_shortcode()            8          Hack pour exécuter le shortcode [embed] avant wp_autop
+        // $wp_embed->autoembed()                8          Embed automatique pour les urls reconnues
     }
 }
