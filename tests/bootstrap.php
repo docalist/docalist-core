708ede17a028aed64517d316f60cf2b37d5d9c6a
Daniel Ménard <daniel.menard.35@gmail.com>
2023-10-20 12:02:59 +0200

    tests/bootstrap : plus besoin de WordPress

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,21 @@
 <?php
-// Environnement de test
-$GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array(
-        'docalist-core/docalist-core.php'
-    ),
-);
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// wordpress-tests doit être dans le include_path de php
-// sinon, modifier le chemin d'accès ci-dessous
-require_once 'wordpress-develop/tests/phpunit/includes/bootstrap.php';
+// // Environnement de test
+// $GLOBALS['wp_tests_options'] = array(
+//     'active_plugins' => array(
+//         'docalist-core/docalist-core.php'
+//     ),
+// );
+
+// // wordpress-tests doit être dans le include_path de php
+// // sinon, modifier le chemin d'accès ci-dessous
+// require_once 'wordpress-develop/tests/phpunit/includes/bootstrap.php';
+
+// function apply_filters($hook_name, $value, ...$args)
+// {
+//     return $value;
+// }
+
+// wp_styles() : utilisé dans Theme.php
+// wp_scripts() : utilisé dans Theme.php
