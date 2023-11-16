0a63ff1c72ebc327efaa2bc9bcf82bf6ac631f9e
Daniel Ménard <daniel.menard.35@gmail.com>
2023-11-16 20:06:14 +0100

    MAJ config php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,7 +3,21 @@
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
-        'global_namespace_import' => ['import_classes' => true, 'import_constants' => true, 'import_functions' => true],
-        'no_superfluous_phpdoc_tags' => ['remove_inheritdoc' => false]
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true
+        ],
+        'no_superfluous_phpdoc_tags' => [
+            'remove_inheritdoc' => false
+        ],
+        'binary_operator_spaces' => [
+            'default'   => 'single_space',
+            'operators' => [
+                // '=' => 'align_single_space',
+                //'=>' => 'align_single_space',
+                '=>' => 'align_single_space_minimal_by_scope',
+            ],
+        ],
     ])
 ;
