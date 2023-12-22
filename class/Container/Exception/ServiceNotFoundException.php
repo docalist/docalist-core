a5d34bfced88da3a29c813e4b8d977eaa68627c4
Daniel Ménard <daniel.menard.35@gmail.com>
2023-12-22 09:02:21 +0100

    Exceptions générées par le container

---
+++ class/Container/Exception/ServiceNotFoundException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of Docalist Core.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Container\Exception;
+
+use InvalidArgumentException;
+use Psr\Container\NotFoundExceptionInterface;
+
+/**
+ * Exception "service non trouvé".
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class ServiceNotFoundException extends InvalidArgumentException //implements NotFoundExceptionInterface
+{
+}
