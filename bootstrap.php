0a23e60d3fbe611104a6a14dec923fd674eecef1
Daniel Ménard <daniel.menard.35@gmail.com>
2023-12-22 13:08:16 +0100

    Nouveau fichier bootstrap.php
    Ce fichier est chargé automatiquement par composer
    lorsqu'on inclut son fichier autoload.
    
    Le rôle du fichier bootstrap est de déclarer l'extensions kernel
    utilisée par le plugin.

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Docalist Core.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+declare(strict_types=1);
+
+namespace Docalist\Core;
+
+use Docalist\Kernel\Kernel;
+
+Kernel::registerExtension(DocalistCoreExtension::class);
