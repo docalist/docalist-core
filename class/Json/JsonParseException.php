2ee6e0b786acfb4ace02de69c427dcf63fbd8c1d
Daniel Ménard <daniel.menard.35@gmail.com>
2025-04-11 09:23:26 +0200

    JsonParseException : arg types

--- class/Json/JsonParseException.php
+++ class/Json/JsonParseException.php
@@ -20,7 +20,7 @@ use RuntimeException;
  */
 class JsonParseException extends RuntimeException
 {
-    public function __construct($message, $line = 0, $col = 0)
+    public function __construct(string $message, int $line = 0, int $col = 0)
     {
         parent::__construct(sprintf('JSON error line %d, column %d: %s.', $line, $col, $message));
     }
